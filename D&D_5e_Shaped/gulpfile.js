09a8447e2ea8e961da98c2589c82003022328016
Mark <mlenser@gmail.com>
2015-05-21 11:05:57 +0200

    weapons compiled via gulp

--- D&D_5e_Shaped/gulpfile.js
+++ D&D_5e_Shaped/gulpfile.js
@@ -3,31 +3,39 @@ var gulp = require('gulp'),
 	inject = require('gulp-inject');
 
 
+var numberOfWeapons = 7;
 
-gulp.task('include', function() {
+
+gulp.task('compile', function() {
 	gulp.src('precompiled/D&D_5e.html')
-			.pipe( include() )
-			.pipe( inject(gulp.src(['precompiled/components/meleewpn.html']), {
-				starttag: '<!-- inject:meleewpn:{{ext}} -->',
-				transform: function (filePath, file) {
-					var template = file.contents.toString('utf8');
-					var s = "";
-					for (var i = 0; i < 7; i++) {
-						s += template.replace(/\x7B\x7Bnum\x7D\x7D/g, i.toString());
+		.pipe( include() )
+		.pipe( inject(gulp.src(['precompiled/components/weapons/melee.html']), {
+			starttag: '<!-- inject:melee:{{ext}} -->',
+			transform: function (filePath, file) {
+				var template = file.contents.toString('utf8'),
+					s = '';
+				for (var i = 0; i < numberOfWeapons; i++) {
+					s += template.replace(/\x7B\x7Bnum\x7D\x7D/g, i.toString())
+					if(i !== numberOfWeapons - 1) {
+						s += '\n\n';
 					}
-					return s;
 				}
-			}))
-			.pipe( inject(gulp.src(['precompiled/components/rangedwpn.html']), {
-				starttag: '<!-- inject:rangedwpn:{{ext}} -->',
-				transform: function (filePath, file) {
-					var template = file.contents.toString('utf8');
-					var s = "";
-					for (var i = 0; i < 7; i++) {
-						s += template.replace(/\x7B\x7Bnum\x7D\x7D/g, i.toString());
+				return s;
+			}
+		}))
+		.pipe( inject(gulp.src(['precompiled/components/weapons/ranged.html']), {
+			starttag: '<!-- inject:ranged:{{ext}} -->',
+			transform: function (filePath, file) {
+				var template = file.contents.toString('utf8'),
+					s = '';
+				for (var i = 0; i < numberOfWeapons; i++) {
+					s += template.replace(/\x7B\x7Bnum\x7D\x7D/g, i.toString());
+					if(i !== numberOfWeapons - 1) {
+						s += '\n\n';
 					}
-					return s;
 				}
-			}))
-			.pipe( gulp.dest('./') )
+				return s;
+			}
+		}))
+		.pipe( gulp.dest('./') )
 });
