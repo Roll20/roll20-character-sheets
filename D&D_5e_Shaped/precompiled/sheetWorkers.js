7a815cb21483bc2e27f5e317f6195e1be80ac724
Mark <mlenser@gmail.com>
2016-01-17 17:42:25 +0100

    inventory weights

--- D&D_5e_Shaped/precompiled/sheetWorkers.js
+++ D&D_5e_Shaped/precompiled/sheetWorkers.js
@@ -1,19 +1,61 @@
 on('change:cp change:sp change:ep change:gp change:pp', function () {
 	getAttrs(['cp', 'copper_per_gold', 'sp', 'silver_per_gold', 'ep', 'electrum_per_gold', 'gp', 'pp', 'platinum_per_gold'], function (v) {
-		var copperPieces = parseInt(v.cp, 10) || 0;
-		var silverPieces = parseInt(v.sp, 10) || 0;
-		var electrumPieces = parseInt(v.ep, 10) || 0;
-		var goldPieces = parseInt(v.gp, 10) || 0;
-		var platinumPieces = parseInt(v.pp, 10) || 0;
-		var copperPerGold = parseInt(v.copper_per_gold, 10) || 100;
-		var silverPerGold = parseInt(v.copper_per_gold, 10) || 10;
-		var electrumPerGold = parseInt(v.copper_per_gold, 10) || 2;
-		var platinumPerGold = parseInt(v.platinum_per_gold, 10) || 10;
+		var copperPieces = parseFloat(v.cp) || 0;
+		var silverPieces = parseFloat(v.sp) || 0;
+		var electrumPieces = parseFloat(v.ep) || 0;
+		var goldPieces = parseFloat(v.gp) || 0;
+		var platinumPieces = parseFloat(v.pp) || 0;
+		var copperPerGold = parseFloat(v.copper_per_gold) || 100;
+		var silverPerGold = parseFloat(v.copper_per_gold) || 10;
+		var electrumPerGold = parseFloat(v.copper_per_gold) || 2;
+		var platinumPerGold = parseFloat(v.platinum_per_gold) || 10;
 		var totalGold = (copperPieces / copperPerGold) + (silverPieces / silverPerGold) + (electrumPieces / electrumPerGold) + goldPieces + (platinumPieces * platinumPerGold);
 		var coinWeight = (copperPieces + silverPieces + electrumPieces + goldPieces + platinumPieces) / 50;
 		setAttrs({
 			total_gp: totalGold.toFixed(2),
 			coin_weight: coinWeight
+		});
+	});
+});
+
+on('change:repeating_inventory', function () {
+	setAttrs({
+		'weight_inventory': 0
+	});
+
+	getSectionIDs('repeating_inventory', function (ids) {
+		var inventoryArray = [];
+		var finalSetAttrs = {};
+
+		for (var i = 0; i < ids.length; i++) {
+			inventoryArray.push('repeating_inventory_' + ids[i] + '_carried');
+			inventoryArray.push('repeating_inventory_' + ids[i] + '_qty');
+			inventoryArray.push('repeating_inventory_' + ids[i] + '_weight');
+		}
+
+		getAttrs(inventoryArray, function (v) {
+			var sum = 0;
+			console.log('v', v);
+			console.log('ids', ids);
+			for (var j = 0; j < ids.length; j++) {
+				var carried = v['repeating_inventory_' + ids[j] + '_carried'] || true;
+				if(carried) {
+					var qty = parseInt(v['repeating_inventory_' + ids[j] + '_qty'], 10) || 1;
+					console.log('qty', qty);
+					var weight = parseFloat(v['repeating_inventory_' + ids[j] + '_weight']) || 0;
+					console.log('weight', weight);
+					var totalWeight = qty * weight;
+					console.log('totalWeight', totalWeight);
+
+
+					sum += totalWeight;
+					console.log('sum', sum);
+				}
+			}
+			console.log('total sum', sum);
+
+			finalSetAttrs.weight_inventory = sum;
+			setAttrs(finalSetAttrs);
 		});
 	});
 });
