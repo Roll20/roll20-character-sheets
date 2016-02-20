a7e1e43db4f996e21da0662c0618297dcdb0187c
Mark <mlenser@gmail.com>
2016-02-20 15:43:44 +0100

    hide languages and senses too

--- D&D_5e_Shaped/precompiled/components/sheetWorkers.js
+++ D&D_5e_Shaped/precompiled/components/sheetWorkers.js
@@ -2405,12 +2405,34 @@ function updateSenses () {
 	var finalSetAttrs = {};
 
 	getAttrs(collectionArray, function (v) {
-		finalSetAttrs.senses = lowercaseWords(v.senses);
+		if (v.senses) {
+			finalSetAttrs.senses_exist = 1;
+			finalSetAttrs.senses = lowercaseWords(v.senses);
+		} else {
+			finalSetAttrs.senses_exist = 0;
+		}
 		setFinalAttrs(v, finalSetAttrs);
 	});
 }
 on('change:senses', function () {
 	updateSenses();
+});
+
+function updateLanguages () {
+	var collectionArray = ['languages'];
+	var finalSetAttrs = {};
+
+	getAttrs(collectionArray, function (v) {
+		if (v.languages) {
+			finalSetAttrs.languages_exist = 1;
+		} else {
+			finalSetAttrs.languages_exist = 0;
+		}
+		setFinalAttrs(v, finalSetAttrs);
+	});
+}
+on('change:languages', function () {
+	updateLanguages();
 });
 
 function updateSpeed () {
@@ -2447,6 +2469,8 @@ function updateDamageVulnerabilities () {
 		if (v.damage_vulnerabilities) {
 			finalSetAttrs.damage_vulnerabilities_exist = 1;
 			finalSetAttrs.damage_vulnerabilities = lowercaseDamageTypes(v.damage_vulnerabilities);
+		} else {
+			finalSetAttrs.damage_vulnerabilities_exist = 0;
 		}
 		setFinalAttrs(v, finalSetAttrs);
 	});
@@ -2462,6 +2486,8 @@ function updateDamageResistances () {
 		if (v.damage_resistances) {
 			finalSetAttrs.damage_resistances_exist = 1;
 			finalSetAttrs.damage_resistances = lowercaseDamageTypes(v.damage_resistances);
+		} else {
+			finalSetAttrs.damage_resistances_exist = 0;
 		}
 		setFinalAttrs(v, finalSetAttrs);
 	});
@@ -2479,6 +2505,8 @@ function updateDamageImmunities () {
 		if (v.damage_immunities) {
 			finalSetAttrs.damage_immunities_exist = 1;
 			finalSetAttrs.damage_immunities = lowercaseDamageTypes(v.damage_immunities);
+		} else {
+			finalSetAttrs.damage_immunities_exist = 0;
 		}
 		setFinalAttrs(v, finalSetAttrs);
 	});
@@ -2495,6 +2523,8 @@ function updateConditionImmunities () {
 		if (v.condition_immunities) {
 			finalSetAttrs.condition_immunities_exist = 1;
 			finalSetAttrs.condition_immunities = lowercaseDamageTypes(v.condition_immunities);
+		} else {
+			finalSetAttrs.condition_immunities_exist = 0;
 		}
 		setFinalAttrs(v, finalSetAttrs);
 	});
