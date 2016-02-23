1c8e782acda353b9d2328e097726032e8368616d
Filip Čapek <capekfilip@capekfilip.cz>
2016-02-23 23:07:30 +0100

    saving throw bonus fix
    saving throw roll didn’t take bonuses to calculate total number

--- D&D_5e_Reshaped/precompiled/components/sheetWorkers.js
+++ D&D_5e_Reshaped/precompiled/components/sheetWorkers.js
@@ -2047,7 +2047,7 @@ function updateSavingThrow (ability) {
 		if (proficiency === '@{PB}') {
 			var pb = getIntValue(v.pb);
 			total += pb;
-			totalFormula += pb + '[proficient]';
+			totalFormula += ADD + pb + '[proficient]';
 		}
 
 		var abilitySavingThrowBonus = getIntValue(v[ability + '_save_bonus']);
