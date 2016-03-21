9275e92e2ceaaea6aa039b13229a68b2b5fc0ab2
Justin Florentine <justin.f.florentine@espn.com>
2016-03-21 14:54:55 -0400

    excludes logging and initiative avoidance

--- Shadowrun5e-Gerdofal/GlitchCatcher.js
+++ Shadowrun5e-Gerdofal/GlitchCatcher.js
@@ -1,22 +1,20 @@
 on('chat:message', function (msg) {
     
-    if((msg.type == 'general' && (msg.rolltemplate == 'sr1' || msg.rolltemplate == 'sr2')) || msg.type == 'rollresult') { //todo JSON.parse(msg.content) to get the rollresult obj
-        //log(msg);
+    if((msg.type == 'general' && (msg.rolltemplate.indexOf('sr1') >= 0)) || msg.type == 'rollresult') { //todo JSON.parse(msg.content) to get the rollresult obj
         
         if(msg.type == 'rollresult') {
             var rollResult = JSON.parse(msg.content);
-            //log(rollResult);
+            log(rollResult);
             var poolSize = rollResult.rolls[0].dice;
             var dice = rollResult.rolls[0].results;
         } else {
-            //log(msg.inlinerolls);
+            log(msg.inlinerolls);
             var poolSize = msg.inlinerolls[1].results.rolls[0].dice;
             var dice = msg.inlinerolls[1].results.rolls[0].results;
         }
-        //log(poolSize + " dice rolled");
-        //log(dice);
-        if(poolSize == null || poolSize <= 1) {
-            return;
+        
+        if(msg.content.indexOf('Initiative') >= 0) {
+            return; //not a pooled roll, don't report glitches
         }
         var oneCount = 0;
         var hitCount = 0;
@@ -34,10 +32,10 @@ on('chat:message', function (msg) {
         }
         log(msg.who + " rolled " + oneCount +" out of " + poolSize);
         if(oneCount > poolSize/2 && hitCount == 0) {
-            //log(msg.who + " critically glitched");
+            log(msg.who + " critically glitched");
             sendChat("player|"+msg.playerid, "/direct <big><strong>CRIT GLITCH!</strong></big>");
         } else if(oneCount > poolSize/2){
-            //log(msg.who + " glitched");
+            log(msg.who + " glitched");
             sendChat("player|"+msg.playerid, "/direct <big><strong>GLITCH!</strong></big>");
         }
     }
