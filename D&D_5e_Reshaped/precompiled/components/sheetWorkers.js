3333ef6204f55dfc7edbda8c61a9b38a0deb5b94
Mark <mlenser@gmail.com>
2016-04-07 22:12:30 +0200

    version 2.4.1

--- D&D_5e_Reshaped/precompiled/components/sheetWorkers.js
+++ D&D_5e_Reshaped/precompiled/components/sheetWorkers.js
@@ -1,7 +1,7 @@
 /* global setAttrs:false, getAttrs:false, on:false, getSectionIDs:false, generateRowID:false */
 'use strict';
 
-const currentVersion = '2.4.0';
+const currentVersion = '2.4.1';
 let TRANSLATIONS;
 const SKILLS = {
   acrobatics: 'dexterity',
