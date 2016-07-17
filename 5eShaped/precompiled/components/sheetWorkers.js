dfbce96008f3eec62bb7a44b060dcf22802a9602
Mark <mlenser@gmail.com>
2016-07-18 00:11:58 +0200

    version 5.2.1

--- 5eShaped/precompiled/components/sheetWorkers.js
+++ 5eShaped/precompiled/components/sheetWorkers.js
@@ -1,7 +1,7 @@
 /* global setAttrs:false, getAttrs:false, on:false, getSectionIDs:false, generateRowID:false, getTranslationByKey:false */
 'use strict';
 
-const currentVersion = '5.2.0';
+const currentVersion = '5.2.1';
 const SKILLS = {
   ACROBATICS: 'dexterity',
   ANIMALHANDLING: 'wisdom',
