f006dd6fdee11ca8b87cc7d68bfc1c073644aebb
Mark <mlenser@gmail.com>
2016-10-22 18:21:53 +0200

    force everything to upgrade

--- 5eShaped/precompiled/scripts/Upgrade.js
+++ 5eShaped/precompiled/scripts/Upgrade.js
@@ -416,35 +416,24 @@ export class Upgrade {
       },
     });
   }
+  isPositiveInteger(x) {
+    return /^\d+$/.test(x);
+  }
+  validateParts(parts) {
+    for (let i = 0; i < parts.length; ++i) {
+      if (!this.isPositiveInteger(parts[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+  versionCompare(v1, v2) {
+    const v1parts = v1.split('.');
+    const v2parts = v2.split('.');
 
-  versionCompare(v1, v2, options) {
-    const lexicographical = options && options.lexicographical;
-    const zeroExtend = options && options.zeroExtend;
-    let v1parts = v1.split('.');
-    let v2parts = v2.split('.');
-
-    const isValidPart = (x) => {
-      return (lexicographical ? /^\d+[A-Za-z]*$/ : /^\d+$/).test(x);
-    };
-
-    if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
+    if (!this.validateParts(v1parts) || !this.validateParts(v2parts)) {
       return NaN;
     }
-
-    if (zeroExtend) {
-      while (v1parts.length < v2parts.length) {
-        v1parts.push('0');
-      }
-      while (v2parts.length < v1parts.length) {
-        v2parts.push('0');
-      }
-    }
-
-    if (!lexicographical) {
-      v1parts = v1parts.map(Number);
-      v2parts = v2parts.map(Number);
-    }
-
     for (let i = 0; i < v1parts.length; ++i) {
       if (v2parts.length === i) {
         return 1;
@@ -452,20 +441,18 @@ export class Upgrade {
 
       if (v1parts[i] === v2parts[i]) {
         continue;
-      } else if (v1parts[i] > v2parts[i]) {
-        return 1;
-      } else {
-        return -1;
       }
-    }
-
-    if (v1parts.length !== v2parts.length) {
+      if (v1parts[i] > v2parts[i]) {
+        return 1;
+      }
       return -1;
     }
-
+    if (v1parts.length != v2parts.length) {
+      return -1;
+    }
     return 0;
   }
-
+  
   upgrade() {
     if (this.versionCompare(currentVersion, '2.1.0') < 0) {
       npc.updateChallenge();
@@ -607,14 +594,10 @@ export class Upgrade {
       spells.updateHasSpellPoints();
       character.updateLevels();
     }
-    if (this.versionCompare(currentVersion, '6.10.0') < 0) {
+    if (this.versionCompare(currentVersion, '6.10.4') < 0) {
       spells.updateChatMacro();
-    }
-    if (this.versionCompare(currentVersion, '6.10.1') < 0) {
       this.updateWarlockMaxLevelOrdinal();
       spells.updateSheetList();
-    }
-    if (this.versionCompare(currentVersion, '6.10.3') < 0) {
       attacks.update();
       abilityChecks.updateInitiative();
     }
