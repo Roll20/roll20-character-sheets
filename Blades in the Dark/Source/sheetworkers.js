b04ff6ac230f69587ee883cee1fc578586cf0cf8
joesinghaus <deceptive.duality@gmail.com>
2017-08-25 20:06:48 +0200

    Blades in the Dark v1.13: Actually bump the version

--- Blades in the Dark/Source/sheetworkers.js
+++ Blades in the Dark/Source/sheetworkers.js
@@ -2312,8 +2312,8 @@ on('sheet:opened', () => {
 		v.version ? upgradeSheet(v.version) : initialiseSheet();
 		// Set version number
 		setAttrs({
-			version: '1.12',
-			character_sheet: 'Blades in the Dark v1.12'
+			version: '1.13',
+			character_sheet: 'Blades in the Dark v1.13'
 		});
 	});
 });
