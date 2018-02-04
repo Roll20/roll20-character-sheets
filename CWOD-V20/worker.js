6fec19cbde7cc273b71fa8dd44a7e3a149d22242
James Goppert <james.goppert@gmail.com>
2018-02-03 23:32:30 -0500

    Improve formatting and add base point calcs for discp. backgrounds.

--- CWOD-V20/worker.js
+++ CWOD-V20/worker.js
@@ -1,7 +1,8 @@
+"use strict";
 const attribute_types = data.attribute_types;
 const ability_types = data.ability_types;
-const attributes = [].concat(...attribute_types.map(x => data[x]))
-const abilities = [].concat(...ability_types.map(x => data[x]))
+const attributes = [].concat(...attribute_types.map(x => data[x]));
+const abilities = [].concat(...ability_types.map(x => data[x]));
 const disciplines = data.disciplines;
 const backgrounds = data.backgrounds;
 const virtues = data.virtues;
@@ -24,6 +25,9 @@ const prop_base = properties.map(x => x + "_base");
 const prop_free = properties.map(x => x + "_free");
 const prop_xp = properties.map(x => x + "_xp");
 const xp_deps = [].concat(prop_base, prop_free, prop_xp, ["clan"]);
+const lists = [attributes, abilities, disciplines,
+	backgrounds, virtues];
+const types = ["attribute", "ability", "discipline", "background", "virtue"];
 
 // change arg lists
 function list_changed(l) {
@@ -35,42 +39,84 @@ function xpCost(b, f, xp, k, new_cost) {
 	var cost = 0;
 	if (xp > 0) {
 		cost += k*(b + f - 1)*xp + k*xp*(xp + 1)/2;
-		if ((b + f) == 0) {
+		if ((b + f) === 0) {
 			cost += new_cost;
 		}
 	}
 	return cost;
-};
+}
 
 function update_free_spent() {
 	getAttrs(xp_deps, function(v) {
+		console.log("updating free spent");
 		var cost = 0;
-		var lists = [attributes, abilities, disciplines,
-			backgrounds, virtues];
-		var types = ["attribute", "ability", "discipline", "background", "virtue"];
 		for (l in lists) {
-			for (x in lists[l]) { 
+			for (x in lists[l]) {
 				var name = lists[l][x];
 				var type = types[l];
 				var f = parseInt(v[name + "_free"], 10);
 				cost += f*data.free_cost[type];
-			};
-		};
+			}
+		}
 		setAttrs({
 			free_spent: cost
 		});
 	});
 }
 
+function update_base() {
+	getAttrs(prop_base, function(v) {
+		console.log("updating base spent");
+		d_set = {};
+		for (l in lists) {
+			var type = types[l];
+			if (type === "attribute") {
+				for (x in attribute_types) {
+					var attr_type = attribute_types[x];
+					var base = 0;
+					for (y in data[attr_type]) {
+						var name = data[attr_type][y];
+						base += parseInt(v[name + "_base"], 10);
+					}
+					d_set["base_" + attr_type] = base - 3;
+				}
+			} else if (type === "ability") {
+				for (x in ability_types) {
+					var abil_type = ability_types[x];
+					var base = 0;
+					for (y in data[abil_type]) {
+						var name = data[abil_type][y];
+						base += parseInt(v[name + "_base"], 10);
+					}
+					d_set["base_" + abil_type] = base;
+				}
+			} else if (type === "virtue") {
+				var base = 0;
+				for (x in lists[l]) {
+					var name = lists[l][x];
+					base += parseInt(v[name + "_base"], 10);
+				}
+				d_set["base_" + type] = base - 3;
+			} else {
+				var base = 0;
+				for (x in lists[l]) {
+					var name = lists[l][x];
+					base += parseInt(v[name + "_base"], 10);
+				}
+				d_set["base_" + type] = base;
+			}
+		}
+		setAttrs(d_set);
+	});
+}
+
+
 function update_xp_spent() {
 	getAttrs(xp_deps, function(v) {
-		//console.log("updating xp spent");
+		console.log("updating xp spent");
 		var cost = 0;
-		var lists = [attributes, abilities, disciplines,
-			backgrounds, virtues];
-		var types = ["attribute", "ability", "discipline", "background", "virtue"];
 		for (l in lists) {
-			for (x in lists[l]) { 
+			for (x in lists[l]) {
 				var name = lists[l][x];
 				var b = parseInt(v[name + "_base"], 10);
 				var f = parseInt(v[name + "_free"], 10);
@@ -79,25 +125,24 @@ function update_xp_spent() {
 				// get parameters
 				var type = types[l];
 				var k = data.mult_xp_cost[type];
-				if (k == undefined) {
+				if (k === undefined) {
 					console.error("undefined k for type", type);
 					return 0;
-				};
+				}
 				var new_cost = data.new_xp_cost[type];
-				if (new_cost == undefined) {
+				if (new_cost === undefined) {
 					console.error("undefined new_cost for type", type);
 					return 0;
-				};
+				}
 
-				var delta_cost = xpCost(b, f, xp, k, new_cost);
-				cost += delta_cost
-			};
-		};
+				cost += xpCost(b, f, xp, k, new_cost);
+			}
+		}
 		setAttrs({
 			xp_spent: cost
 		});
 	});
-};
+}
 
 on("sheet:opened", function() {
 	console.log("sheet opened");
@@ -123,6 +168,7 @@ on(list_changed(prop_xp), function(){
 
 on(list_changed(prop_base), function(){
 	console.log("base change");
+	update_base();
 });
 
 on(list_changed(xp_deps), function(){
@@ -134,9 +180,9 @@ on("change:clan", function(){
 	console.log("clan change");
 	// initialize to clan disciplines
 	getAttrs([].concat(discipline_names, ["clan"]), function(v) {
-		d_set = {}
+		var d_set = {};
 		for(d in discipline_names) {
-			var discipline_name = v[discipline_names[d]]
+			var discipline_name = v[discipline_names[d]];
 			console.log("discipline name", discipline_names[d], discipline_name);
 			if (d < clan_disciplines[v.clan].length) {
 				clan_discipline = clan_disciplines[v.clan][d];
