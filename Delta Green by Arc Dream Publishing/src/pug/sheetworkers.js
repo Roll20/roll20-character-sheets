ae9bbcaa60aaa299d5bdc3b8a2c2bb745fa1818f
Cassie <cassielevett@gmail.com>
2019-09-15 13:19:25 -0500

    Saving Hit Points Promise

--- Delta Green by Arc Dream Publishing/src/pug/sheetworkers.js
+++ Delta Green by Arc Dream Publishing/src/pug/sheetworkers.js
@@ -1,47 +1,61 @@
+	// === ATTRIBUTE ARRAYS
+		const arrays = {
+			stats: ['strength', 'constitution', 'dexterity', 'intelligence', 'power', 'charisma'],
+			hit_points: ['strength_score', 'constitution_score'],
+			sanity_points: ['power_score'],
+			breaking_point: ['sanity_score', 'power_score'],
+			willpower_points: ['power_score'],
+			repeating_settings: [''],
+			sheets: ['settings'],
+			toggles: ['sheet']
+		};
 
 	// === SHARED FUNCTIONS
 	const setAttributes = (update, silent) => { 
 		(silent === true) ? setAttrs(update, {silent:true}) : setAttrs(update); 
 	};
 
-	// === ATTRIBUTE ARRAYS
-		const arrays = {
-			stats: ['strength', 'constitution', 'dexterity', 'intelligence', 'power', 'charisma'],
-			hit_points: ['strength', 'constitution'],
-			sanity_points: ['power'],
-			breaking_point: ['sanity', 'power'],
-			willpower_points: ['power'],
-			repeating_settings: [''],
-			sheets: ['settings'],
-			toggles: ['sheet']
-		};
-
+	// === GET ATTRIBUTES
+	const getAttributes = (array) => {
+		return new Promise((resolve, reject)  => {
+			try {
+				getAttrs(array, (values) => {
+					resolve(values);
+				});
+			} catch (error) {
+				reject(`ERROR: ${error}`);
+			}
+		});
+	};
 
 	// === STAT SCORES
 	arrays['stats'].forEach(attr => {
 		on(`change:${attr}_score`, (eventinfo) => {
 			const score = parseInt(eventinfo.newValue) || 0;
-
-			setAttributes({attr: score * 5}, true);
+			setAttributes({[`${attr}`]: score * 5}, true);
 		});
 	});
 
-	// === DERIVED ATTRIBUTES
-	/*arrays['hit_points'].forEach(attr => {
+	// === HIT POINTS
+	arrays['hit_points'].forEach(attr => {
 		on(`change:${attr}`, (eventinfo) => {
-			const attributes = arrays['hit_points'];
-			attributes.push('hit_points');
-			getAttrs(attributes, (value) => {
-				const strength = parseInt(value.strength) || 0, constitution = parseInt(value.constitution) || 0; 
+			const attributes = arrays['hit_points'].concat(['hit_points']);
+			getAttributes(attributes).then(values => {
+				const strength = parseInt(values.strength_score) || 0, constitution = parseInt(values.constitution_score) || 0; 
 				const calculated = Math.ceil((strength + constitution)/2);
 				let update = { hit_points_max: calculated }
-				if (value.hit_points === undefined) { update['hit_points'] = calculated; }
-				setAttributes(update, true);
+				if (values.hit_points === undefined || values.hit_points === "") { update['hit_points'] = calculated }
+			  	console.log(`%c HIT POINTS UPDATE`, 'color: green; font-weight:bold');
+			  	console.log(update);
+
+			  	return update
+			}).then(result => {
+				console.log(`%c HIT POINTS RESULT`, 'color: green; font-weight:bold');
+			  	console.log(result);
+			  	setAttributes(result, true);
 			});
 		});
-	})
-
-*/
+	});
 
 	// === TOGGLE SETTINGS
 		arrays['sheets'].forEach(attr => {
@@ -58,7 +72,7 @@
 	// === TOGGLE INPUT SETTINGS
 	    arrays['toggles'].forEach(attr => {
 	        on(`clicked:toggle_${attr}`, () => {
-	        	getAttrs(['toggles'], (value) => {
+	        	getAttrs(['toggles'], (values) => {
 	 				let string = toggleBuilder(attr, v['toggles']);
 	                setAttributes({toggles: string}, true);
 	        	});
@@ -73,14 +87,14 @@
 
 	// === GM WHISPER TOGGLES
 		on('clicked:whisper', (eventinfo) => {
-			getAttrs(['gm_toggle'], (value) => {
-				setAttributes({gm_toggle: (v.gm_toggle.includes('gm')) ?  ' ' : '/w gm'}, true);
+			getAttrs(['gm_toggle'], (values) => {
+				setAttributes({gm_toggle: (values.gm_toggle.includes('gm')) ?  ' ' : '/w gm'}, true);
 			});
 		});
 
 	// === SHEET VERSIONING
 		on('sheet:opened', () => {
-			getAttrs(['version'], (value) => { versioning(parseFloat(v.version) || 1); });
+			getAttrs(['version'], (values) => { versioning(parseFloat(values.version) || 1); });
 		});			
 
 		const versioning = (version) => {
