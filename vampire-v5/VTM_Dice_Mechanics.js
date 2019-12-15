9202cb91bdc8f0fd9d718069a8024cf512370664
Kirintale <momtahk@gmail.com>
2019-12-15 18:10:47 +0000

    Version change notation

--- vampire-v5/VTM_Dice_Mechanics.js
+++ vampire-v5/VTM_Dice_Mechanics.js
@@ -1,4 +1,4 @@
-// Vampire the Masquerade 5e Dice Mechanics by Momtahan K (Version 1.2).
+// Vampire the Masquerade 5e Dice Mechanics by Momtahan K (Version 1.3).
 //
 // The following code is an adaptation of that produced by Roll20 user Konrad J. for "Hero Quest Dice Mechanics". 
 // Many thanks for providing this code free to use.
@@ -27,6 +27,8 @@
 // try to use it for commerical gain. This has been added to ensure the community benefits from it.
 
 // Versions
+// Version 1.3
+// Bug fix. Macros previously may have caused issues, removing some error checking to fix this issue.
 // Version 1.2
 // Updated images to a different location. Fixed bugs where players name would not appear for specific rolls. Fixed bug in which speech marks in a players name would cause issues.
 // Version 1.1
