8ddf57890d06b7d2911bc9a30beaa376fb942b6d
Cassie <cassielevett@gmail.com>
2020-03-15 00:39:17 -0500

    remove excess code

--- Shadowrun5thEdition/src/js/newScripts.js
+++ Shadowrun5thEdition/src/js/newScripts.js
@@ -1,92 +1,64 @@
 
 const updateWounds = () => {
-  try {
-    getAttrs(sheetAttribues.woundCalculation, attrs => {
-      attrs = processingFunctions.parseIntegers(attrs)
-      const sum = processingFunctions.shadowrun.calculateWounds(attrs)
-      processingFunctions.setAttributes({wounds: sum})
-    })
-  } catch (error) {
-    console.error(error)
-  }
+  getAttrs(sheetAttribues.woundCalculation, attrs => {
+    attrs = processingFunctions.parseIntegers(attrs)
+    const sum = processingFunctions.shadowrun.calculateWounds(attrs)
+    processingFunctions.setAttributes({wounds: sum})
+  })
 }
 
 const updateConditionTracks= conditionTrack => {
-  try {
-    getAttrs(sheetAttribues[conditionTrack], attrs => {
-      attrs = processingFunctions.shadowrun.conditionFactor(attrs)
-      const conditionTotal = processingFunctions.shadowrun.calculateConditionTracks(attrs)
-      processingFunctions.setAttributes({[`${conditionTrack}_max`]: conditionTotal}, true)
-    })
-  } catch (error) {
-    console.error(error)
-  }
+  getAttrs(sheetAttribues[conditionTrack], attrs => {
+    attrs = processingFunctions.shadowrun.conditionFactor(attrs)
+    const conditionTotal = processingFunctions.shadowrun.calculateConditionTracks(attrs)
+    processingFunctions.setAttributes({[`${conditionTrack}_max`]: conditionTotal}, true)
+  })
 }
 
 const translations = () => {
-  try {
-    const attributes = sheetAttribues.translationsAttributes
-    const translations = processingFunctions.getTranslations(attributes)
-    let attribute_roll = `?{${translations.attribute}`
-    delete translations.attribute
-    for (let [key, value] of Object.entries(translations)) {
-        attribute_roll += key !== 'none' ? `|${value},@{${key}}` : `|${value},0}`
-    }
-    processingFunctions.setAttributes({attribute_roll: attribute_roll})
-  } catch (error) {
-    console.error(error)
+  const attributes = sheetAttribues.translationsAttributes
+  const translations = processingFunctions.getTranslations(attributes)
+  let attribute_roll = `?{${translations.attribute}`
+  delete translations.attribute
+  for (let [key, value] of Object.entries(translations)) {
+      attribute_roll += key !== 'none' ? `|${value},@{${key}}` : `|${value},0}`
   }
+  processingFunctions.setAttributes({attribute_roll: attribute_roll})
 } 
 
 const updateShotsFired = trigger => {
-  try {
-    getAttrs([`shots_fired`], attrs => {   
-      attrs = processingFunctions.parseIntegers(attrs)
-      const shots = processingFunctions.shadowrun.shotsFired(attrs.shots_fired, trigger)
-      processingFunctions.setAttributes({shots_fired: shots})
-    });
-  } catch (error) {
-    console.error(error)
-  }
+  getAttrs([`shots_fired`], attrs => {   
+    attrs = processingFunctions.parseIntegers(attrs)
+    const shots = processingFunctions.shadowrun.shotsFired(attrs.shots_fired, trigger)
+    processingFunctions.setAttributes({shots_fired: shots})
+  });
 }
 
 const updateAmmoWithShotsFired = () => {
-  try {
-    getAttrs([`primary_range_weapon_ammo`, `shots_fired`], attrs => {
-      attrs = processingFunctions.parseIntegers(attrs)
-      attrs.shots_fired = processingFunctions.convertIntegerNegative(attrs.shots_fired)
-      processingFunctions.setAttributes({primary_range_weapon_ammo: processingFunctions.sumIntegers(Object.values(attrs))})
-    });
-  } catch (error) {
-    console.error(error)
-  }
+  getAttrs([`primary_range_weapon_ammo`, `shots_fired`], attrs => {
+    attrs = processingFunctions.parseIntegers(attrs)
+    attrs.shots_fired = processingFunctions.convertIntegerNegative(attrs.shots_fired)
+    processingFunctions.setAttributes({primary_range_weapon_ammo: processingFunctions.sumIntegers(Object.values(attrs))})
+  });
 }
 
 const updateAmmoWithMax = () => {
-  try {
-    getAttrs([`primary_range_weapon_ammo_max`], attrs => {
-      attrs = processingFunctions.parseIntegers(attrs)
-      processingFunctions.setAttributes({primary_range_weapon_ammo: attrs.primary_range_weapon_ammo_max})
-    });
-  } catch (error) {
-    console.error(error)
-  }
+  getAttrs([`primary_range_weapon_ammo_max`], attrs => {
+    attrs = processingFunctions.parseIntegers(attrs)
+    processingFunctions.setAttributes({primary_range_weapon_ammo: attrs.primary_range_weapon_ammo_max})
+  });
 }
 
 const updateTab = attr => processingFunctions.setAttributes({tab: attr})
 
 const updateAttributes = (array, attribute) => {
-  try {
-    getAttrs(array, attrs => {
-      attrs = processingFunctions.shadowrun.attributeFactory(attrs)
-      processingFunctions.setAttributes({
-        [attribute]: attrs.total,
-        [`display_${attribute}`]: attrs.base === attrs.total ? attrs.base : `${attrs.base} (${attrs.total}}`
-      })
+  getAttrs(array, attrs => {
+    attrs = processingFunctions.shadowrun.attributeFactory(attrs)
+    processingFunctions.setAttributes({
+      [attribute]: attrs.total,
+      [`display_${attribute}`]: attrs.base === attrs.total ? attrs.base : `${attrs.base} (${attrs.total}}`
     })
-  } catch (error) {
-    console.error(error)
-  }
+  })
 }
 
 const updateLimitTotal = attrs => {
@@ -95,46 +67,34 @@ const updateLimitTotal = attrs => {
 }
 
 const updateLimits = attributeLimit => {
-  try {
-    const array = sheetAttribues[attributeLimit].concat([`${attributeLimit}_modifier`, `${attributeLimit}_temp`, `${attributeLimit}_temp_flag`])
-    getAttrs(array, attrs => {
-        attrs = processingFunctions.shadowrun.attributeFactory(attrs)
-        const bonus = attrs.bonus
-        delete attrs.bonus
-        const base = processingFunctions.shadowrun.updateLimitTotal(attrs)
-        processingFunctions.setAttributes({
-          [attributeLimit]: processingFunctions.sumIntegers([base, bonus])
-        })
-    })
-  } catch (error) {
-    console.error(error)
-  }
+  const array = sheetAttribues[attributeLimit].concat([`${attributeLimit}_modifier`, `${attributeLimit}_temp`, `${attributeLimit}_temp_flag`])
+  getAttrs(array, attrs => {
+      attrs = processingFunctions.shadowrun.attributeFactory(attrs)
+      const bonus = attrs.bonus
+      delete attrs.bonus
+      const base = processingFunctions.shadowrun.updateLimitTotal(attrs)
+      processingFunctions.setAttributes({
+        [attributeLimit]: processingFunctions.sumIntegers([base, bonus])
+      })
+  })
 }
 
 const updateMovement = () => {
-  try {
-    getAttrs(['agility', 'walk_modifier', 'run_modifier'], attrs => {
-      attrs = processingFunctions.parseIntegers(attrs)
-      processingFunctions.setAttributes({
-        walk: processingFunctions.shadowrun.calculateWalkSpeed(attrs.agility, attrs.walk_modifier),
-        run: processingFunctions.shadowrun.calculateRunSpeed(attrs.agility, attrs.run_modifier)
-      })
+  getAttrs(['agility', 'walk_modifier', 'run_modifier'], attrs => {
+    attrs = processingFunctions.parseIntegers(attrs)
+    processingFunctions.setAttributes({
+      walk: processingFunctions.shadowrun.calculateWalkSpeed(attrs.agility, attrs.walk_modifier),
+      run: processingFunctions.shadowrun.calculateRunSpeed(attrs.agility, attrs.run_modifier)
     })
-  } catch (error) {
-     console.error(error)
-  }
+  })
 }
 
 const updateDerivedAttribute = derivedAttribute => {
-  try {
-    getAttrs(sheetAttribues[derivedAttribute], attrs => {
-      attrs = processingFunctions.shadowrun.attributeFactory(attrs)
-      const sum = processingFunctions.sumIntegers(Object.values(attrs))
-      processingFunctions.setAttributes({[derivedAttribute]: sum})
-    })
-  } catch (error) {
-    console.error(error)
-  }
+  getAttrs(sheetAttribues[derivedAttribute], attrs => {
+    attrs = processingFunctions.shadowrun.attributeFactory(attrs)
+    const sum = processingFunctions.sumIntegers(Object.values(attrs))
+    processingFunctions.setAttributes({[derivedAttribute]: sum})
+  })
 }
 
 //for Mocha Unit Texting
