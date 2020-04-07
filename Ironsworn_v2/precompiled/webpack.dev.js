5f9bc13a2030ae22e1422ed3781ba206d0839426
LeeRhodes <leerhodes@protonmail.com>
2020-04-07 23:56:18 +0100

    added working asset template

--- Ironsworn_v2/precompiled/webpack.dev.js
+++ Ironsworn_v2/precompiled/webpack.dev.js
@@ -1,19 +1,41 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 var path = require('path');
 
 module.exports = {
   entry: {
     index: './html/Ironsworn.html',
-    styles: './css/Ironsworn.css'
+    styles: './src/Ironsworn.styl'
   },
   mode: 'development',
   devServer: {
+    hot: true,
+    watchOptions: {
+      poll: true
+    },
     port: 8080
   },
   output: {
     path: path.resolve(__dirname, './dist')
   },
+  module: {
+    rules: [
+      {
+        test: /\.styl(us)?$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'stylus-loader'
+        ]
+      },
+    ],
+  },
   plugins: [
+    new OptimizeCSSAssetsPlugin({}),
+    new FixStyleOnlyEntriesPlugin(),
+    new MiniCssExtractPlugin({filename: "Ironsworn.css"}),
     new HtmlWebpackPlugin({
       template: './html/Ironsworn.html',
       minify: true,
--- Ironsworn_v2/precompiled/webpack.prod.js
+++ Ironsworn_v2/precompiled/webpack.prod.js
@@ -1,16 +1,30 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 var path = require('path');
 
 module.exports = {
   entry: {
     index: './html/Ironsworn.html',
-    styles: './css/Ironsworn.css'
+    styles: './src/Ironsworn.styl'
   },
   mode: 'production',
   output: {
     path: path.resolve(__dirname, './dist')
   },
+  module: {
+    rules: [
+      {
+        test: /\.styl(us)?$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'stylus-loader'
+        ]
+      },
+    ],
+  },
   plugins: [
+    new MiniCssExtractPlugin({filename: "Ironsworn.css"}),
     new HtmlWebpackPlugin({
       template: './html/Ironsworn.html',
       minify: true,
