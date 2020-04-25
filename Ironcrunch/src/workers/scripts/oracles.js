d86583a60cd5035c6e54c1a19a19d8111281f0db
LeeRhodes <leerhodes@protonmail.com>
2020-04-25 21:30:06 +0100

    fixed delve oracle preview

--- Ironcrunch/src/workers/scripts/oracles.js
+++ Ironcrunch/src/workers/scripts/oracles.js
@@ -4,6 +4,12 @@ on('change:oracle_core_button', function(eventinfo) {
   });
 });
 
+on('change:oracle_delve_button', function(eventinfo) {
+  setAttrs({
+    oracle_delve: eventinfo.newValue,
+  });
+});
+
 on('change:oracle_page_button', function(eventinfo) {
   setAttrs({
     oracle_page: eventinfo.newValue,
