1c7f3e5e087bd6f4de0c5052ba76128ac96adb3c
Tetrakern <infi@infinitynet.de>
2020-09-27 04:22:16 +0200

    dice parser now captures X+ and X- references as well

--- Savage Worlds - Graph Paper/source/scripts/workers.js
+++ Savage Worlds - Graph Paper/source/scripts/workers.js
@@ -83,6 +83,8 @@ var TETRA = TETRA || ( function() {
     _.each(_.keys(parseCodes), (k) => {
       code = code.replaceAll(`+${k}`, `+${parseCodes[k]}`);
       code = code.replaceAll(`-${k}`, `-${parseCodes[k]}`);
+      code = code.replaceAll(`${k}-`, `${parseCodes[k]}+`);
+      code = code.replaceAll(`${k}-`, `${parseCodes[k]}-`);
     });
 
     return code;
