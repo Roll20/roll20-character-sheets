bc34421380aca29942067453ace9d02ca5c85d80
Riernar <Riernar@users.noreply.github.com>
2021-01-14 13:32:16 +0100

    Ars Magica 5th improvements

--- Ars_Magica_5th/Readme.md
+++ Ars_Magica_5th/Readme.md
@@ -1,5 +1,17 @@
 # Ars Magica 5th
 
+## Usage
+The final sheet file `Ars_Magica_5th.html` is generated from a template. To modify it:
+- Modify `template.html`
+    - You can use python code to generate repeating parts. See `generate.py` and `parts/__init__.py` and below for details and helper code
+- Run `generate.py` (requires python 3.8+)
+
+### Using `generate.py`
+The script will read `template.html` and replace parts of the form `$$identifier$$` by the value found under that identifier in the dictionary `part` defined in `parts/__init__.py`. This makes it possible to use python code to generate repeating parts of the sheet. You can find examples in `__init__.py`.
+
+It is not necessary to put all code in `parts/__init__.py`. You can use other files, import the helpers you need from `parts/helpers.py`, and then import the final value in `parts/__init__.py` and assign it to an identifier.
+
+If you are unfamiliar with python, you can write everythin manually in `template.html`, though modifying what is already python-generated will require looking at the code.
 
 ## Translation Notes
 
---
+++ Ars_Magica_5th/parts/translations.py
@@ -0,0 +1,38 @@
+from .helpers import CHARACTERISTICS, FORMS, TECHNIQUES
+
+TRANSLATION_ATTRS = (
+    [
+        ("ability", "Abiliy", "ability"),
+        ("armor", "Armor", "armor"),
+        ("artes", "Artes Lib.", "artes-lib-"),
+        ("attack", "Attack", "attack"),
+        ("aura", "Aura", "aura"),
+        ("bonus", "Bonus", "bonus"),
+        ("circumstances", "Circumstances", "circumstances-m"),
+        ("circumstantial", "Circumstancial", "circumstantial"),
+        ("damage", "Damage", "damage"),
+        ("defense", "Defense", "defense"),
+        ("encumbrance", "Encumbrance", "encumbrance"),
+        ("fatigue", "Fatigue", "fatigue-m"),
+        ("form", "Form", "form"),
+        ("initiative", "Initiative", "initiative"),
+        ("modifiers", "Modifiers", "modifiers"),
+        ("philos", "Philos.", "philos-"),
+        ("query", "Name of Query", "query"),
+        ("technique", "Technique", "technique"),
+        ("wounds", "Wounds", "wounds"),
+    ]
+    + [(char.lower(), char.capitalize(), char.lower()) for char in CHARACTERISTICS]
+    + [(tech.lower(), tech.capitalize(), tech.lower()) for tech in TECHNIQUES]
+    + [(form.lower(), form.capitalize(), form.lower()) for form in FORMS]
+)
+
+translation_attrs = "\n".join(
+    """<input name ="attr_%s_i18n" type="hidden" value="%s" />""" % (attr, default)
+    for attr, default, tkey in TRANSLATION_ATTRS
+)
+
+translation_attrs_setup = "\n".join(
+    """%s_i18n: getTranslationByKey("%s")""" % (attr, tkey)
+    for attr, default, tkey in TRANSLATION_ATTRS
+)
