bdeb2be0366d7e91b3b087bcdb38a87da3da35e6
Peter Bjerg Mogensen <pbm@impact.dk>
2021-02-05 16:44:28 +0100

    - Moved wizard spell list to data lists
    - Pulled out wizard template to separate file for later inserts. Easier to handle all sections
    - Updated combine file to also handle adding correct white spacing for inserts

--- AD&D 2E Revised/build/combine.ps1
+++ AD&D 2E Revised/build/combine.ps1
@@ -2,14 +2,31 @@
 $sourceFolder = 'D:\git\roll20-character-sheets\AD&D 2E Revised'
 $outputFile = 'D:\git\roll20-character-sheets\AD&D 2E Revised\2ESheet.html'
 
+$replaceConstant = '#REPLACE#'
+
 $content = Get-Content -Path $inputFile
 
 $inserts = ($content | Select-String -Pattern 'insert_')
 
 $inserts | ForEach-Object {
-   $fileName = $_.Line.Split('_')[1]
+   $_
+   $split = $_.Line.Split('_')
+   $whiteSpace = $split[0].Replace('insert', '')
+   $fileName = $split[1]
    $file = Get-ChildItem -Path $sourceFolder -Filter $fileName -Recurse
-   $content = $content -replace $_.Line, (Get-Content -Path $file.FullName -Raw)
+   $fileContent = Get-Content -Path $file.FullName
+   if ($whiteSpace.Length -gt 0) {
+      for ($i = 0; $i -lt $fileContent.Length; $i++)
+      {
+         $fileContent[$i] = $whiteSpace + $fileContent[$i]
+      }
+   }
+   $rawContent = $fileContent -join "`n"
+   if ($split.Length -gt 2 -and $rawContent.Contains($replaceConstant)) {
+      $rawContent = $rawContent.Replace($replaceConstant, $split[2])
+   }
+
+   $content = $content -replace $_.Line, ($rawContent)
 }
 
 $content | Set-Content -Path $outputFile
