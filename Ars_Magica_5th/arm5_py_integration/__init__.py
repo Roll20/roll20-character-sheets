c3b432e4402acfa7896a378d49fda39ec69a7aed
Riernar <Riernar@users.noreply.github.com>
2021-02-12 10:27:38 +0100

    added option to ask which charact to use for abilities

--- Ars_Magica_5th/arm5_py_integration/__init__.py
+++ Ars_Magica_5th/arm5_py_integration/__init__.py
@@ -135,17 +135,18 @@ GLOBALS["characteristic_name_options"] = repeat_template(
     CHARACTERISTICS,
     str_key="char",
 )
-GLOBALS["characteristic_name_ask"] = (
-    '<option class="sheet-italic" value="[[?{@{characteristic_i18n}|'
+GLOBALS["characteristic_name_ask_attr"] = (
+    "?{@{characteristic_i18n}|"
     + "| ".join(
-        "@{%(char)s_i18n},%(char)s" % {"char": char} for char in CHARACTERISTICS
+        "@{%(char)s_i18n},@{%(char)s_Score} [@{%(char)s_i18n}]" % {"char": char}
+        for char in CHARACTERISTICS
     )
-    + '}]]" data-i18n="ask">Ask</option>'
+    + "}"
 )
 
 # Abilities
-ability_roll_template = "&{template:ability} {{name= @{character_name}}} {{label4=^{circumstances-m}}} {{result4=[[(?{@{circumstantial_i18n}|0})]]}} {{label0=@{Ability_name}}} {{banner=@{Ability_Speciality}}} {{label1=^{rank}}} {{result1= [[ @{Ability_Score} + @{Ability_Puissant} ]]}} {{label2=@{Ability_CharacName}}} {{result2=[[@{sys_at}@{character_name}@{sys_pipe}@{Ability_CharacName}_Score@{sys_lbk}]]}} {{label3=^{weakness-m}}} {{result3=[[ ([[floor(@{Fatigue})]]) [@{fatigue_i18n}] + (@{wound_total}) [@{wounds_i18n}] ]]}} {{result0=%(roll)s}} {{botch-button=[@{botch_i18n}!](~@{character_name}|botch)}} {{crit-button=[@{critical_i18n}!](~@{character_name}|critical)}}"
-ability_roll = "[[ %(die)s + (@{Ability_Score} + @{Ability_Puissant}) [@{Ability_name}] + ([[@{sys_at}@{character_name}@{sys_pipe}@{Ability_CharacName}_Score@{sys_lbk}]]) [@{Ability_CharacName}] + (@{wound_total}) [@{wounds_i18n}] + ([[floor(@{Fatigue})]]) [@{fatigue_i18n}] + (?{@{circumstantial_i18n}|0}) [@{circumstances_i18n}] ]]"
+ability_roll_template = "&{template:ability} {{name= @{character_name}}} {{label0=@{Ability_name}}} {{banner=@{Ability_Speciality}}} {{label1=^{rank}}} {{result1= [[ @{Ability_Score} + @{Ability_Puissant} ]]}} {{label2=@{Ability_CharacName}}} {{result2=[[@{sys_at}@{character_name}@{sys_pipe}@{Ability_CharacName}_Score@{sys_lbk}]]}} {{label3=^{weakness-m}}} {{result3=[[ ([[floor(@{Fatigue})]]) [@{fatigue_i18n}] + (@{wound_total}) [@{wounds_i18n}] ]]}} {{label4=^{circumstances-m}}} {{result4=[[(?{@{circumstantial_i18n}|0})]]}} {{result0=%(roll)s}} {{botch-button=[@{botch_i18n}!](~@{character_name}|botch)}} {{crit-button=[@{critical_i18n}!](~@{character_name}|critical)}}"
+ability_roll = "[[ %(die)s + (@{Ability_Score} + @{Ability_Puissant}) [@{Ability_name}] + (@{sys_at}@{character_name}@{sys_pipe}@{Ability_CharacName}_Score@{sys_lbk}) [@{sys_at}@{character_name}@{sys_pipe}@{Ability_CharacName}_i18n@{sys_lbk}] + (@{wound_total}) [@{wounds_i18n}] + ([[floor(@{Fatigue})]]) [@{fatigue_i18n}] + (?{@{circumstantial_i18n}|0}) [@{circumstances_i18n}] ]]"
 GLOBALS["ability_roll_simple"] = ability_roll_template % {
     "roll": ability_roll % {"die": "@{simple-die}"}
 }
