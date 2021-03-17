f91ab9ed5face37603da3d39482709ebd8385a18
Valanor <Coolmander@gmail.com>
2021-03-17 09:46:32 +0100

    implementing Template Workflow for Nunjucks

---
+++ Shadowrun6th-German/app.js
@@ -0,0 +1,8 @@
+import nunjucks from "nunjucks";
+import { writeFileSync } from 'fs';
+
+var msg="Test";
+nunjucks.configure({ autoescape: true });
+msg=nunjucks.render('template.html',{});
+writeFileSync('test.html', msg);
+console.log("Write file");
