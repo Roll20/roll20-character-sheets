8a797a57fd8773c2e5c1aec2315885c79377d403
Andreas Johansson <andreas.johansson@aalto.fi>
2021-05-14 22:54:36 +0300

    MythicD6 add npc sheet, update page navigation

--- MythicD6/src/sheetworker.js
+++ MythicD6/src/sheetworker.js
@@ -16,7 +16,46 @@ const miscstats = ["stress_points", "stress_points_max", "hero_points", "hero_po
 const mainattributesnmisc = ["coordination", "knowledge", "perception", "physique", "presence", "reflexes", "fighting"];
 
 
-//TAS.config({logging:{debug: true}});
+// tabs
+
+const buttonlist = ["notes","settings","npc","character"];
+buttonlist.forEach(button => {
+    on(`clicked:${button}`, function() {
+
+        setAttrs({
+            sheetTab: button
+        });
+    });
+});
+
+on("sheet:opened", function() {
+
+  getAttrs(["npc", "sheetTab"], values => {
+
+    let npc = int(values["npc"]);
+    let sheetTab = values["sheetTab"];
+    console.log("sheetTab: ", sheetTab);
+    //let page = "character";
+
+    if (sheetTab === "character"){
+      npc = 0;
+      console.log("sheetTab was char, set NPC=0: ", npc);
+    }
+    else if (sheetTab === "npc"){
+      npc = 1;
+      console.log("sheetTab was npc, set NPC=1: ", npc);
+    }
+    else if (sheetTab === "notes"){
+      console.log("sheetTab was note");
+    }
+    else if (sheetTab === "settings"){
+      console.log("sheetTab was settings");
+    }
+    setAttrs({
+        npc: npc
+    });
+  });
+});
 
 // update misc stats
 
