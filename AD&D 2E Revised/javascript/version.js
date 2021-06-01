d859d7f5d8fbf8af485a5f21a3c073d25d046f91
Peter Bjerg Lidegaard <pbm@impact.dk>
2021-06-01 11:27:03 +0200

    Updated priest Heat Metal spell to use table for its formating

--- AD&D 2E Revised/javascript/version.js
+++ AD&D 2E Revised/javascript/version.js
@@ -1,7 +1,7 @@
 // --- Version change start --- //
 
 const sheetName = 'AD&D 2E Revised';
-const sheetVersion = '4.0.0';
+const sheetVersion = '4.1.0';
 
 function moveStaticToRepeating(section, fieldsToMove) {
     getAttrs(fieldsToMove, function (values) {
@@ -38,29 +38,7 @@ on('sheet:opened', function(){
                 announcement: 1
             },{silent:true});
             
-            moveStaticToRepeating('weaponprofs', ['weapprofname', 'weapprofnum', 'expert', 'specialist', 'mastery', 'high-mastery', 'grand-mastery', 'chosen-weapon']);
-            moveStaticToRepeating('profs', ['profname', 'profslots', 'profstatnum', 'profmod']);
-            moveStaticToRepeating('langs', ['langname', 'lang-rw']);
-            moveStaticToRepeating('gear', ['geardesc', 'gearweight', 'gearqty', 'gearloc']);
-            moveStaticToRepeating('gear-stored', ['gear-stored-desc', 'gear-stored-weight', 'gear-stored-qty', 'gear-stored-loc', 'on-mount']);
-            moveStaticToRepeating('magicres', ['magresname', 'magrestar', 'magresmod', 'magresnotes']);
             
-            //ranged weapons
-            moveStaticToRepeating('weapons2', ['weaponname2', 'strbonus2', 'dexbonus2', 'prof-level2', 'range-mod-attack', 'attacknum2', 'attackadj2', 'ThAC02', 'crit-thresh2', 'range2', 'size2', 'weaptype-slash2', 'weaptype-pierce2', 'weaptype-blunt2', 'weapspeed2']);
-            moveStaticToRepeating('ammo', ['ammoname', 'strbonus3', 'dexbonus3', 'specialist-damage2', 'mastery-damage2', 'damadj2', 'damsm2', 'daml2', 'knockdown2', 'ammoremain']);
-
-            //melee damage weapons
-            moveStaticToRepeating('weapons', ['weaponname', 'strbonus', 'dexbonus', 'prof-level', 'attacknum', 'attackadj', 'ThAC0', 'crit-thresh', 'range', 'size', 'weaptype-slash', 'weaptype-pierce', 'weaptype-blunt', 'weapspeed']);
-            moveStaticToRepeating('weapons-damage', ['weaponname1', 'strbonus1', 'dexbonus1', 'specialist-damage', 'mastery-damage', 'damadj', 'damsm', 'daml', 'knockdown1']);
-
-            getAttrs(['nonprof-penalty'], function(values) {
-                let nonprof = Math.abs(parseInt(values['nonprof-penalty'])) * -1;
-                let famil = Math.floor(nonprof / 2)
-                setAttrs({
-                    ['nonprof-penalty']: nonprof,
-                    ['famil-penalty']: famil
-                });
-            });
         }
     });
 });
