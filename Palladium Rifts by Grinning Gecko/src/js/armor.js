1dcc23c09aa7a0298a07be6e980e2c15bd7971ad
Garth Gutenberg <garth.gutenberg@gmail.com>
2021-08-18 21:37:30 -0500

    Remove some logging

--- Palladium Rifts by Grinning Gecko/src/js/armor.js
+++ Palladium Rifts by Grinning Gecko/src/js/armor.js
@@ -5,34 +5,27 @@ async function applyDamageToNextActiveArmor(section, currentRowId, damage) {
     `repeating_${section}_${id}_is_active`,
     `repeating_${section}_${id}_currentmdc`,
   ]);
-  console.log(attrKeys);
   const attrs = {};
   attrs[`repeating_${section}_${currentRowId}_currentmdc`] = 0;
   const a = await getAttrsAsync(attrKeys);
-  console.log(a);
   let next = false;
   let nextCurrentMdc = false;
-  console.log(ids);
   for (const id of ids) {
-    console.log(id);
     if (id == currentRowId) {
       next = true;
       continue;
     }
     if (next && Boolean(Number(a[`repeating_${section}_${id}_is_active`]))) {
-      console.log("here");
       nextCurrentMdc = +a[`repeating_${section}_${id}_currentmdc`] - damage;
       attrs[`repeating_${section}_${id}_currentmdc`] = nextCurrentMdc;
       break;
     }
   }
-  console.log("done for", nextCurrentMdc);
   if (nextCurrentMdc === false) {
     nextCurrentMdc =
       +a[`repeating_${section}_${currentRowId}_currentmdc`] - damage;
     attrs[`repeating_${section}_${currentRowId}_currentmdc`] = nextCurrentMdc;
   }
-  console.log(attrs);
   await setAttrsAsync(attrs);
 }
 
@@ -48,13 +41,13 @@ on("clicked:repeating_armor:applydamage", async (e) => {
     +a[`repeating_armor_${rowId}_damage`];
   if (currentMdc < 0) {
     const [r, section, rowId] = e.sourceAttribute.split("_");
+    // apply to next active in line
+    // if there is no next in line, show as negative
     await applyDamageToNextActiveArmor(
       section,
       rowId.toLowerCase(),
       Math.abs(currentMdc)
     );
-    // apply to next active in line
-    // if there is no next in line, show as negative
   } else {
     await setAttrsAsync({
       [`repeating_armor_${rowId}_currentmdc`]: currentMdc,
@@ -66,7 +59,6 @@ on("clicked:repeating_armor:resetmdc", async (e) => {
   console.log("clicked:repeating_armor:resetmdc", e);
   const [r, section, rowId] = e.sourceAttribute.split("_");
   const a = await getAttrsAsync([`repeating_armor_${rowId}_basemdc`]);
-  console.log(a);
   await setAttrsAsync({
     [`repeating_armor_${rowId}_currentmdc`]:
       a[`repeating_armor_${rowId}_basemdc`],
