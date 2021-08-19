8c5050f543f697de833e5aac808d9374ccca5467
Garth Gutenberg <garth.gutenberg@gmail.com>
2021-08-19 16:43:14 -0500

    Move active damage outside of repeating armor, and rely solely on sort order of active rows

--- Palladium Rifts by Grinning Gecko/src/js/armor.js
+++ Palladium Rifts by Grinning Gecko/src/js/armor.js
@@ -1,58 +1,41 @@
-async function applyDamageToNextActiveArmor(section, currentRowId, damage) {
-  console.log("applyDamageToNextActiveArmor", section, currentRowId, damage);
+async function applyDamageToNextActiveArmor(section, damage) {
+  console.log("applyDamageToNextActiveArmor", section, damage);
   const ids = await getSectionIDsOrderedAsync(section);
   const attrKeys = ids.flatMap((id) => [
     `repeating_${section}_${id}_is_active`,
     `repeating_${section}_${id}_currentmdc`,
   ]);
   const attrs = {};
-  attrs[`repeating_${section}_${currentRowId}_currentmdc`] = 0;
   const a = await getAttrsAsync(attrKeys);
-  let next = false;
-  let nextCurrentMdc = false;
-  for (const id of ids) {
-    if (id == currentRowId) {
-      next = true;
-      continue;
+  let damageCarry = damage;
+  // let nextCurrentMdc = false;
+  let currentMdc = false;
+
+  const activeIds = ids.reduce((acc, cur) => {
+    if (Boolean(Number(a[`repeating_${section}_${cur}_is_active`]))) {
+      acc.push(cur);
     }
-    if (next && Boolean(Number(a[`repeating_${section}_${id}_is_active`]))) {
-      nextCurrentMdc = +a[`repeating_${section}_${id}_currentmdc`] - damage;
-      attrs[`repeating_${section}_${id}_currentmdc`] = nextCurrentMdc;
+    return acc;
+  }, []);
+
+  let i = 0;
+  for (const id of activeIds) {
+    currentMdc = +a[`repeating_${section}_${id}_currentmdc`] - damageCarry;
+    if (currentMdc < 0 && ++i < activeIds.length) {
+      attrs[`repeating_${section}_${id}_currentmdc`] = 0;
+      damageCarry = Math.abs(currentMdc);
+    } else {
+      attrs[`repeating_${section}_${id}_currentmdc`] = currentMdc;
       break;
     }
-  }
-  if (nextCurrentMdc === false) {
-    nextCurrentMdc =
-      +a[`repeating_${section}_${currentRowId}_currentmdc`] - damage;
-    attrs[`repeating_${section}_${currentRowId}_currentmdc`] = nextCurrentMdc;
   }
   await setAttrsAsync(attrs);
 }
 
-on("clicked:repeating_armor:applydamage", async (e) => {
-  console.log("clicked:repeating_armor:applydamage", e);
-  const [r, section, rowId] = e.sourceAttribute.split("_");
-  const a = await getAttrsAsync([
-    `repeating_armor_${rowId}_currentmdc`,
-    `repeating_armor_${rowId}_damage`,
-  ]);
-  let currentMdc =
-    +a[`repeating_armor_${rowId}_currentmdc`] -
-    +a[`repeating_armor_${rowId}_damage`];
-  if (currentMdc < 0) {
-    const [r, section, rowId] = e.sourceAttribute.split("_");
-    // apply to next active in line
-    // if there is no next in line, show as negative
-    await applyDamageToNextActiveArmor(
-      section,
-      rowId.toLowerCase(),
-      Math.abs(currentMdc)
-    );
-  } else {
-    await setAttrsAsync({
-      [`repeating_armor_${rowId}_currentmdc`]: currentMdc,
-    });
-  }
+on("clicked:armorapplydamage", async (e) => {
+  console.log("clicked:armorapplydamage", e);
+  const a = await getAttrsAsync([`armordamage`]);
+  await applyDamageToNextActiveArmor("armor", +a[`armordamage`]);
 });
 
 on("clicked:repeating_armor:resetmdc", async (e) => {
