4ba7a74cfd51b3369597685750b49b93e98eefcf
Garth Gutenberg <garth.gutenberg@gmail.com>
2021-09-06 16:15:30 -0500

    Templated roll queries where it matters

--- Palladium Rifts by Grinning Gecko/src/js/definitions.js
+++ Palladium Rifts by Grinning Gecko/src/js/definitions.js
@@ -558,17 +558,11 @@ const COMBAT_KEYS = [
 ];
 const SAVE_KEYS_ATTRIBUTE_BONUSES = {
   me_bonus: ["psionics", "insanity"],
-  pe_bonus: [
-    "magic",
-    "lethalpoison",
-    "nonlethalpoison",
-    "disease",
-    "pain",
-    "drugs",
-  ],
+  pe_bonus: ["magic", "lethalpoison", "nonlethalpoison", "disease", "drugs"],
   pe_coma_bonus: ["comadeath"],
   perception_bonus: ["perceptioncheck"],
   none: [
+    "pain",
     "horrorfactor",
     "mindcontrol",
     "illusions",
