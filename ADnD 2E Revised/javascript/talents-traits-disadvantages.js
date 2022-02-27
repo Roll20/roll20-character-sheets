57b69143798f9a839c3b1cf7ca69d146670b3659
Peter Bjerg Lidegaard <dbf.black.bull@gmail.com>
2022-02-27 11:51:22 +0100

    - Added critical hit buttons for players option weapons. - Added taletns from PO C&T to a file, but not implemented on the sheet yet

---
+++ ADnD 2E Revised/javascript/talents-traits-disadvantages.js
@@ -0,0 +1,136 @@
+//#region Player's Options: Combat & Tactics
+const talents = {};
+talents['Alertness'] = {
+    'slots': 1,
+    'abilityScore': 'Wisdom/Intuition',
+    'modifier': +1,
+    'classes': 'Special Talent',
+    'book': 'Player\'s Options: Combat & Tactics'
+};
+talents['Ambidexterity'] = {
+    'slots': 1,
+    'abilityScore': 'Dexterity/Aim',
+    'modifier': 0,
+    'classes': 'Warrior/Rogue, Special Talent',
+    'book': 'Player\'s Options: Combat & Tactics'
+};
+talents['Ambush'] = {
+    'slots': 1,
+    'abilityScore': 'Intelligence/Reason',
+    'modifier': 0,
+    'classes': 'Warrior/Rogue, Special Talent',
+    'book': 'Player\'s Options: Combat & Tactics'
+};
+talents['Camouflage'] = {
+    'slots': 1,
+    'abilityScore': 'Intelligence/Learning',
+    'modifier': 0,
+    'classes': 'Warrior/Rogue, Special Talent',
+    'book': 'Player\'s Options: Combat & Tactics'
+};
+talents['Dirty Fighting'] = {
+    'slots': 1,
+    'abilityScore': 'Intelligence/Learning',
+    'modifier': +1,
+    'classes': 'Warrior/Rogue, Special Talent',
+    'book': 'Player\'s Options: Combat & Tactics'
+};
+talents['Endurance'] = {
+    'slots': 2,
+    'abilityScore': 'Constitution/Toughness',
+    'modifier': 0,
+    'classes': 'Warrior, Special Talent',
+    'book': 'Player\'s Options: Combat & Tactics'
+};
+talents['Fine Balance'] = {
+    'slots': 2,
+    'abilityScore': 'Dexterity/Agility',
+    'modifier': 0,
+    'classes': 'Warrior/Rogue, Special Talent',
+    'book': 'Player\'s Options: Combat & Tactics'
+};
+talents['Iron Will'] = {
+    'slots': 2,
+    'abilityScore': 'Wisdom/Will',
+    'modifier': -2,
+    'classes': 'Warrior/Priest, Special Talent',
+    'book': 'Player\'s Options: Combat & Tactics'
+};
+talents['Leadership'] = {
+    'slots': 1,
+    'abilityScore': 'Charisma/Leadership',
+    'modifier': -1,
+    'classes': 'Warrior, Special Talent',
+    'book': 'Player\'s Options: Combat & Tactics'
+};
+talents['Quickness'] = {
+    'slots': 2,
+    'abilityScore': 'Dexterity/Aim',
+    'modifier': 0,
+    'classes': 'Warrior/Rogue, Special Talent',
+    'book': 'Player\'s Options: Combat & Tactics'
+};
+talents['Steady Hand'] = {
+    'slots': 1,
+    'abilityScore': 'Dexterity/Aim',
+    'modifier': 0,
+    'classes': 'Rogue, Special Talent',
+    'book': 'Player\'s Options: Combat & Tactics'
+};
+talents['Trouble Sense'] = {
+    'slots': 1,
+    'abilityScore': 'Wisdom/Intuition',
+    'modifier': 0,
+    'classes': 'Special Talent',
+    'book': 'Player\'s Options: Combat & Tactics'
+};
+talents['Flying Kick'] = {
+    'slots': 1,
+    'abilityScore': 'Strength/Power',
+    'modifier': 0,
+    'classes': 'Warrior, Martial Arts Talents',
+    'book': 'Player\'s Options: Combat & Tactics'
+};
+talents['Flying Kick'] = {
+    'slots': 1,
+    'abilityScore': 'Strength/Power',
+    'modifier': 0,
+    'classes': 'Warrior, Martial Arts Talents',
+    'book': 'Player\'s Options: Combat & Tactics'
+};
+talents['Backward Kick'] = {
+    'slots': 1,
+    'abilityScore': 'N/A',
+    'modifier': 0,
+    'classes': 'Warrior/Priest/Rogue, Martial Arts Talents',
+    'book': 'Player\'s Options: Combat & Tactics'
+};
+talents['Spring'] = {
+    'slots': 1,
+    'abilityScore': 'Dexterity/Agility',
+    'modifier': 0,
+    'classes': 'Warrior/Rogue, Martial Arts Talents',
+    'book': 'Player\'s Options: Combat & Tactics'
+};
+talents['Crushing Blow'] = {
+    'slots': 1,
+    'abilityScore': 'N/A',
+    'modifier': 0,
+    'classes': 'Warrior/Priest/Rogue, Martial Arts Talents',
+    'book': 'Player\'s Options: Combat & Tactics'
+};
+talents['Instant Stand'] = {
+    'slots': 1,
+    'abilityScore': 'Dexterity/Agility',
+    'modifier': 0,
+    'classes': 'Warrior/Priest/Rogue, Martial Arts Talents',
+    'book': 'Player\'s Options: Combat & Tactics'
+};
+talents['Missile Deflection'] = {
+    'slots': 1,
+    'abilityScore': 'N/A',
+    'modifier': 0,
+    'classes': 'Warrior/Priest/Rogue, Martial Arts Talents',
+    'book': 'Player\'s Options: Combat & Tactics'
+};
+//#endregion
