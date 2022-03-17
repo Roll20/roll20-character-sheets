ff836b14ca5c0f79380bbef259431a6969238832
Peter Bjerg Lidegaard <dbf.black.bull@gmail.com>
2022-03-17 16:13:59 +0100

    Added brackets for ability scores

--- ADnD 2E Revised/javascript/players-option-talents.js
+++ ADnD 2E Revised/javascript/players-option-talents.js
@@ -2,98 +2,98 @@
 const talents = {};
 talents['Alertness'] = {
     'slots': 1,
-    'abilityScore': 'Wisdom/Intuition',
+    'abilityScore': '@{Wisdom}/@{Intuition}',
     'modifier': +1,
     'classes': 'Special Talent',
     'book': 'Player\'s Options: Combat & Tactics'
 };
 talents['Ambidexterity'] = {
     'slots': 1,
-    'abilityScore': 'Dexterity/Aim',
+    'abilityScore': '@{Dexterity}/@{Aim}',
     'modifier': 0,
     'classes': 'Warrior/Rogue, Special Talent',
     'book': 'Player\'s Options: Combat & Tactics'
 };
 talents['Ambush'] = {
     'slots': 1,
-    'abilityScore': 'Intelligence/Reason',
+    'abilityScore': '@{Intelligence}/@{Reason}',
     'modifier': 0,
     'classes': 'Warrior/Rogue, Special Talent',
     'book': 'Player\'s Options: Combat & Tactics'
 };
 talents['Camouflage'] = {
     'slots': 1,
-    'abilityScore': 'Intelligence/Learning',
+    'abilityScore': '@{Intelligence}/@{Learning}',
     'modifier': 0,
     'classes': 'Warrior/Rogue, Special Talent',
     'book': 'Player\'s Options: Combat & Tactics'
 };
 talents['Dirty Fighting'] = {
     'slots': 1,
-    'abilityScore': 'Intelligence/Learning',
+    'abilityScore': '@{Intelligence}/@{Learning}',
     'modifier': +1,
     'classes': 'Warrior/Rogue, Special Talent',
     'book': 'Player\'s Options: Combat & Tactics'
 };
 talents['Endurance'] = {
     'slots': 2,
-    'abilityScore': 'Constitution/Toughness',
+    'abilityScore': '@{Constitution}/@{Toughness}',
     'modifier': 0,
     'classes': 'Warrior, Special Talent',
     'book': 'Player\'s Options: Combat & Tactics'
 };
 talents['Fine Balance'] = {
     'slots': 2,
-    'abilityScore': 'Dexterity/Agility',
+    'abilityScore': '@{Dexterity}/@{Agility}',
     'modifier': 0,
     'classes': 'Warrior/Rogue, Special Talent',
     'book': 'Player\'s Options: Combat & Tactics'
 };
 talents['Iron Will'] = {
     'slots': 2,
-    'abilityScore': 'Wisdom/Will',
+    'abilityScore': '@{Wisdom}/@{Will}',
     'modifier': -2,
     'classes': 'Warrior/Priest, Special Talent',
     'book': 'Player\'s Options: Combat & Tactics'
 };
 talents['Leadership'] = {
     'slots': 1,
-    'abilityScore': 'Charisma/Leadership',
+    'abilityScore': '@{Charisma}/@{Leadership}',
     'modifier': -1,
     'classes': 'Warrior, Special Talent',
     'book': 'Player\'s Options: Combat & Tactics'
 };
 talents['Quickness'] = {
     'slots': 2,
-    'abilityScore': 'Dexterity/Aim',
+    'abilityScore': '@{Dexterity}/@{Aim}',
     'modifier': 0,
     'classes': 'Warrior/Rogue, Special Talent',
     'book': 'Player\'s Options: Combat & Tactics'
 };
 talents['Steady Hand'] = {
     'slots': 1,
-    'abilityScore': 'Dexterity/Aim',
+    'abilityScore': '@{Dexterity}/@{Aim}',
     'modifier': 0,
     'classes': 'Rogue, Special Talent',
     'book': 'Player\'s Options: Combat & Tactics'
 };
 talents['Trouble Sense'] = {
     'slots': 1,
-    'abilityScore': 'Wisdom/Intuition',
+    'abilityScore': '@{Wisdom}/@{Intuition}',
     'modifier': 0,
     'classes': 'Special Talent',
     'book': 'Player\'s Options: Combat & Tactics'
 };
 talents['Flying Kick'] = {
     'slots': 1,
-    'abilityScore': 'Strength/Power',
+    'abilityScore': '@{Strength}/@{Power}',
     'modifier': 0,
     'classes': 'Warrior, Martial Arts Talents',
     'book': 'Player\'s Options: Combat & Tactics'
 };
 talents['Flying Kick'] = {
     'slots': 1,
-    'abilityScore': 'Strength/Power',
+    'abilityScore': '@{Strength}/@{Power}',
     'modifier': 0,
     'classes': 'Warrior, Martial Arts Talents',
     'book': 'Player\'s Options: Combat & Tactics'
@@ -107,7 +107,7 @@ talents['Backward Kick'] = {
 };
 talents['Spring'] = {
     'slots': 1,
-    'abilityScore': 'Dexterity/Agility',
+    'abilityScore': '@{Dexterity}/@{Agility}',
     'modifier': 0,
     'classes': 'Warrior/Rogue, Martial Arts Talents',
     'book': 'Player\'s Options: Combat & Tactics'
@@ -121,7 +121,7 @@ talents['Crushing Blow'] = {
 };
 talents['Instant Stand'] = {
     'slots': 1,
-    'abilityScore': 'Dexterity/Agility',
+    'abilityScore': '@{Dexterity}/@{Agility}',
     'modifier': 0,
     'classes': 'Warrior/Priest/Rogue, Martial Arts Talents',
     'book': 'Player\'s Options: Combat & Tactics'
