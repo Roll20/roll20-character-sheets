de29f3d82ee44a791ea3ee2880873308d8967baf
Peter Bjerg Lidegaard <dbf.black.bull@gmail.com>
2022-06-11 22:36:36 +0200

    Added updated datalists for nonweapon proficiencies

--- ADnD_2E_Revised/build/buildPug.js
+++ ADnD_2E_Revised/build/buildPug.js
@@ -6,6 +6,42 @@ const jsFolder = path.join(sourceFolder, 'javascript');
 const pugFolder = path.join(sourceFolder, 'pug');
 const htmlFolder = path.join(sourceFolder, 'html', 'components');
 const miscFolder = path.join(sourceFolder, 'misc');
+const booksPriority = [
+    'PHB',
+    'The Complete Fighter\'s Handbook',
+    'The Complete Thief\'s Handbook',
+    'The Complete Priest\'s Handbook',
+    'The Complete Wizard\'s Handbook',
+    'Tome of Magic',
+    'Arms and Equipment Guide',
+    'The Complete Book of Dwarves',
+    'The Complete Bard\'s Handbook',
+    'The Complete Book of Elves',
+    'The Complete Book of Humanoids',
+    'The Complete Ranger\'s Handbook',
+    'The Complete Paladin\'s Handbook',
+    'The Complete Druid\'s Handbook',
+    'The Complete Barbarian\'s Handbook',
+    'The Complete Book of Necromancers',
+    'The Complete Ninja\'s Handbook',
+    'Player\'s Option: Combat & Tactics',
+    'Player\'s Option: Skills & Powers',
+    'Player\'s Option: Spells & Magic',
+];
+function bookShorthand(books) {
+    return books.map(b => b.replace('The Complete', '')
+        .replace('Book of', '')
+        .replace('Handbook', '')
+        .replace('Player\'s Option:','')
+        .replace('Arms and Equipment Guide', 'A&E')
+        .replace('Tome of Magic','ToM')
+        .replace('Combat & Tactics', 'PO: C&T')
+        .replace('Skills & Powers', 'PO: S&P')
+        .replace('Spells & Magic', 'PO: S&M')
+        .replace('\'','')
+        .trim())
+        .join(', ')
+}
 
 console.log('---- Generating dynamicDatalists ----');
 const spellsJs = ['wizardSpells.js', 'priestSpells.js'];
@@ -30,14 +66,14 @@ console.log('weapons.js');
 console.log('weaponProficiencies.js');
 const weapons = require(path.join(jsFolder, 'weapons.js'));
 const weaponsProficiencies = require(path.join(jsFolder, 'weaponProficiencies.js'));
-html += pug.renderFile(path.join(pugFolder, 'weaponsDatalists.pug'), {pretty: true, data: weapons, proficiencies: weaponsProficiencies});
-
-const nonWeaponProfs = require(path.join(jsFolder, 'nonweaponProficiencies.js'));
-const nonWeaponProfTable = pug.renderFile(path.join(pugFolder, 'nonweaponProficienciesOverview.pug'), {pretty: true, data: nonWeaponProfs});
-fs.writeFileSync(path.join(miscFolder, 'nonweapon-proficiencies-overview.html'), nonWeaponProfTable);
-
+html += pug.renderFile(path.join(pugFolder, 'weaponsDatalists.pug'), {pretty: true, data: weapons, proficiencies: weaponsProficiencies, booksPriority: booksPriority, bookShorthand: bookShorthand});
 let weaponTable = pug.renderFile(path.join(pugFolder, 'weaponsOverview.pug'), {pretty: true, data: weapons});
 fs.writeFileSync(path.join(miscFolder, 'weapons-overview.html'), weaponTable);
+
+const nonWeaponProfs = require(path.join(jsFolder, 'nonweaponProficiencies.js'));
+html += pug.renderFile(path.join(pugFolder, 'nonweaponProficienciesDatalist.pug'), {pretty: true, data: nonWeaponProfs, booksPriority: booksPriority, bookShorthand: bookShorthand})
+const nonWeaponProfTable = pug.renderFile(path.join(pugFolder, 'nonweaponProficienciesOverview.pug'), {pretty: true, data: nonWeaponProfs});
+fs.writeFileSync(path.join(miscFolder, 'nonweapon-proficiencies-overview.html'), nonWeaponProfTable);
 
 const simpleDatalists = [
     ['players-option-talents.js','talents'],
