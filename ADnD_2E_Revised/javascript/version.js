f68caa935d2e2d4da7acb363005650ce10628a43
Peter Bjerg Lidegaard <dbf.black.bull@gmail.com>
2022-06-13 16:28:05 +0200

    Updated version

--- ADnD_2E_Revised/javascript/version.js
+++ ADnD_2E_Revised/javascript/version.js
@@ -1,7 +1,7 @@
 // --- Version change start --- //
 
 const SHEET_NAME = 'AD&D 2E Revised';
-const SHEET_VERSION = '4.9.1';
+const SHEET_VERSION = '4.10.0';
 
 on('sheet:opened', function(){
     getAttrs(['character_sheet'],function(attrs){
