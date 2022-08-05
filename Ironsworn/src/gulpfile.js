b507a7e67e84682f218b734e93151ad13eb52b67
Guillaume Durand <idispatch38@gmail.com>
2022-08-05 20:09:59 +0200

    Test framework

--- Ironsworn/src/gulpfile.js
+++ Ironsworn/src/gulpfile.js
@@ -7,19 +7,33 @@ gulp.task('css', () => {
     .pipe(stylus())
     .pipe(gulp.dest('../'))
 })
- 
+
 gulp.task('html', () => {
   return gulp.src('./app/Ironsworn.pug')
     .pipe(pug({
       pretty: true,
-      locals: { translations: require('../translation.json'), fs: require('fs')}
+      locals: { translations: require('../translation.json'), fs: require('fs') }
     }))
     .pipe(gulp.dest('../'))
 })
 
 gulp.task('watch', gulp.series(['css', 'html'], () => {
   gulp.watch('./app/**/*.styl', gulp.series(['css']))
-  gulp.watch(['./app/**/*.pug','./app/**/*.js'], gulp.series(['html']))
+  gulp.watch(['./app/**/*.pug', './app/**/*.js'], gulp.series(['html']))
 }))
 
-gulp.task('build',  gulp.series(['css','html']))
+gulp.task('build', gulp.series(['css', 'html']))
+
+gulp.task('test-html', () => {
+  return gulp.src('./test/Ironsworn.pug')
+    .pipe(pug({
+      pretty: true,
+      locals: { translations: require('../translation.json'), fs: require('fs') }
+    }))
+    .pipe(gulp.dest('./test'))
+})
+
+gulp.task('test-watch', gulp.series(['css', 'test-html'], () => {
+  gulp.watch('./app/**/*.styl', gulp.series(['css']))
+  gulp.watch(['./app/**/*.pug', './app/**/*.js', './test/**/*.pug', './test/**/*.js'], gulp.series(['test-html']))
+}))
