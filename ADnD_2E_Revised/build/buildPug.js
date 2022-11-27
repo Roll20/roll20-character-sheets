36e6eaabf3249ad898cbc6bb9e1984d20e431066
Peter Bjerg Lidegaard <dbf.black.bull@gmail.com>
2022-11-27 16:37:03 +0100

    Added printing information to ToM

--- ADnD_2E_Revised/build/buildPug.js
+++ ADnD_2E_Revised/build/buildPug.js
@@ -13,7 +13,7 @@ const booksPriority = [
     {book: 'The Complete Priest\'s Handbook', print: 'Seventh printing, October 1996'},
     {book: 'The Complete Wizard\'s Handbook', print: '10th printing, March 1996'},
     {book: 'The Complete Psionics Handbook', print: 'Ninth printing: October 1996'},
-    {book: 'Tome of Magic', print: ''},
+    {book: 'Tome of Magic', print: 'Seventh Printing: March, 1996'},
     {book: 'Arms and Equipment Guide', print: 'Fifth Printing: January 1994'},
     {book: 'The Complete Book of Dwarves', print: 'November 1993'},
     {book: 'The Complete Bard\'s Handbook', print: 'Fourth printing, November 1994'},
