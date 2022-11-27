52fc8b1f4253f9c48681c30cd3278e5f5d9c27de
Peter Bjerg Lidegaard <dbf.black.bull@gmail.com>
2022-11-27 22:28:47 +0100

    Checked the data for the book of elves. Only the special arrows are missing which are annoying to implement

--- ADnD_2E_Revised/build/buildPug.js
+++ ADnD_2E_Revised/build/buildPug.js
@@ -17,7 +17,7 @@ const booksPriority = [
     {book: 'Arms and Equipment Guide', print: 'Fifth Printing: January 1994'},
     {book: 'The Complete Book of Dwarves', print: 'November 1993'},
     {book: 'The Complete Bard\'s Handbook', print: 'Fourth printing, November 1994'},
-    {book: 'The Complete Book of Elves', print: ''},
+    {book: 'The Complete Book of Elves', print: ''}, // checked
     {book: 'The Complete Book of Humanoids', print: 'Sixth printing, March 1999'},
     {book: 'The Complete Ranger\'s Handbook', print: 'Fifth printing, July 1995'},
     {book: 'The Complete Paladin\'s Handbook', print: ''},
