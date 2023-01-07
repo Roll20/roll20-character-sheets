db773a06e5f7e6899fec88ff5aabf91965f9a7a3
Seraaron <40521082+Seraaron@users.noreply.github.com>
2023-01-07 17:31:35 +0000

    Icon v0.3
    - Added preliminary darkmode functionality
    - Added preliminary roll template / macro functionality
    - Partially fixed tab impersistence
    - Fixed burden column scale
    - Added 'roster' tab for foes & allies
    - Added camp fixtures
    - Fixed collapse checkbox global
    - Fixed custom action names resetting
    - Added ability info boxes

--- ICON RPG [MassifPress]/javascript/calculators.js
+++ ICON RPG [MassifPress]/javascript/calculators.js
@@ -38,29 +38,6 @@
     return returnValue;
   };
   k.registerFuncs({calcAttributeMod});//We need to register the function with the k-scaffold so that the k-scaffold's listeners can call it when needed.
-  
-  /**
-   * Our function to calculate our total Defense or reduction_value rating.
-   * 
-   * We are using the new `sections` parameter so that we can loop over our `repeating_armor` rows to determine what defense or reduction_value bonuses to apply.
-   * 
-   * Note that the `trigger` paramter is coming last here. Because the K-scaffold calls functions via the object destructuring argument method, We can put these arguments in any order, as long as they are all part of object. Even though this is possible, I would typically put these in a standardized order of `trigger`,`attributes`,`sections` just to make it easier for me to read the code.
-   * @param {object} attributes - Same as in {@link calcAttributeMod}.
-   * @param {object} sections - An object that contains all of the repeating row information for our sheet.
-   * @param {string[]} sections.repeating_armor - The row IDs of our `repeating_armor` section. We'll use this to loop through and determine which defense bonuses should apply.
-   * @param {object} trigger - As in {@link calcAttributeMod}
-   * @returns {number} - Returns the new defense stat value, which the K-scaffold's infrastructure will apply to the sheet.
-   */
-  const calcDefense = function({attributes,sections,trigger}){
-    let name = trigger.name.replace(/_.+/,'');//Extract the attribute name that will be used for referencing affecting attributes. e.g. `defense_total` becomes `defense` for use in referencing `defence_mod` and `repeating_armor_-oyho8987uLKJ_defense`.
-    let activeArmorIDs = sections.repeating_armor.filter((id)=>attributes[`repeating_armor_${id}_equipped`]);//Because the K-scaffold's attributes object takes care of ensuring a proper return value for our attributes, we can just do a simple check for whether the attribute value is truthy (e.g. `1`) or falsy (e.g. `0`) because this attribute is a checkbox with a checked state of `1` and an off state of `0`.
-    let finesse = name === 'defense' ?//Handles accounting for finesse in the defense calculation.
-      10 + attributes.finesse_mod :
-      0;
-    let activeArmorBonus = activeArmorIDs.reduce((total,id)=>total += attributes[`repeating_armor_${id}_${name}`],0);//Total up the bonuses from all our equipped gear.
-    return attributes[`${name}_mod`] + finesse + activeArmorBonus;//Total our defense or reduction score using the miscellaneous modifier and all of our equipped armors/shields.
-  };
-  k.registerFuncs({calcDefense});//Register the function
 
   const boundNumber = (value, min, max) => {
     [ value, min, max ] = [ value, min, max].map(input => parseFloat(input));
@@ -74,7 +51,7 @@
   on('change:vigor change:vigor_max change:wounds change:vitality', () => {
     getAttrs([ 'vigor', 'vigor_max' ], (attributeValues) => {
       const boundValue = boundNumber(attributeValues.vigor, 0, attributeValues.vigor_max);
-      if (boundValue !== attributeValues.vigor) {
+      if (boundValue === attributeValues.vigor) {
         setAttrs({ vigor: boundValue });
       }
     });
@@ -83,7 +60,7 @@
   on('change:hp change:hp_max change:wounds change:vitality', () => {
     getAttrs([ 'hp', 'hp_max' ], (attributeValues) => {
       const boundValue = boundNumber(attributeValues.hp, 0, attributeValues.hp_max);
-      if (boundValue !== attributeValues.hp) {
+      if (boundValue === attributeValues.hp) {
         setAttrs({ hp: boundValue });
       }
     });
--- ICON RPG [MassifPress]/javascript/triggeredfuncs.js
+++ ICON RPG [MassifPress]/javascript/triggeredfuncs.js
@@ -6,20 +6,20 @@
  * @param {object} attributes - The attributes object as returned from `getAllAttrs` and used in the default K-scaffold event cascade.
  * @returns {void}
  */
-const navigateSheet = function({trigger,attributes}){
-  let name = trigger ?
-    trigger.name :
-    attributes.sheet_state;//The trigger property is not passed when this function is called on sheet:opened. So, we need to get the current sheet state out of memory in that case.
-  let [,,page] = k.parseTriggerName(name);//Parse out the name of the navigation button that was clicked.
-  page = page.replace(/^nav-|-action$/g,'');//Remove the navigation and action button specific pieces of the button name to leave us with just the name of the button, which ideally is the same as a CSS class that we want to apply `.active` to.
-  navButtons.forEach((button)=>{//iterate through all the navigation buttons. navButtons is exported to our JS by the K-scaffold.
-    let element = button.replace(/-action/,'');//sanitize the button name down until we get just the target class name.
-    $20(`.${element}`).removeClass('active');//Remove the active class from all elements with the indicated class (e.g. .character)
-  });
-  $20(`.${page}`).addClass('active');//Add .active back to the elements that we are actually navigating to. We do this outside of the loop so that we can properly handle elements that may have more than one navigation class applied to them, although we don't have any of these in this sheet setup.
-  attributes.sheet_state = page;//Store the new page selection in our sheet_state attribute so that it will be remembered when the sheet is opened again.
-};
-k.registerFuncs({navigateSheet},{type:['opener']});//Register the function. Note that we are using a new argument with registerFuncs. The second object here is the options object where we can define what type of function this is. See the K-scaffold documentation for more information on the available types.
+// const navigateSheet = function({trigger,attributes}){
+//   let name = trigger ?
+//     trigger.name :
+//     attributes.sheet_state;//The trigger property is not passed when this function is called on sheet:opened. So, we need to get the current sheet state out of memory in that case.
+//   let [,,page] = k.parseTriggerName(name);//Parse out the name of the navigation button that was clicked.
+//   page = page.replace(/^nav-|-action$/g,'');//Remove the navigation and action button specific pieces of the button name to leave us with just the name of the button, which ideally is the same as a CSS class that we want to apply `.active` to.
+//   navButtons.forEach((button)=>{//iterate through all the navigation buttons. navButtons is exported to our JS by the K-scaffold.
+//     let element = button.replace(/-action/,'');//sanitize the button name down until we get just the target class name.
+//     $20(`.${element}`).removeClass('active');//Remove the active class from all elements with the indicated class (e.g. .character)
+//   });
+//   $20(`.${page}`).addClass('active');//Add .active back to the elements that we are actually navigating to. We do this outside of the loop so that we can properly handle elements that may have more than one navigation class applied to them, although we don't have any of these in this sheet setup.
+//   attributes.sheet_state = page;//Store the new page selection in our sheet_state attribute so that it will be remembered when the sheet is opened again.
+// };
+// k.registerFuncs({navigateSheet},{type:['opener']});//Register the function. Note that we are using a new argument with registerFuncs. The second object here is the options object where we can define what type of function this is. See the K-scaffold documentation for more information on the available types.
 
 /**
  * Function to control what sections of the sheet are accessible based on the type of sheet that is selected (npc vs. character). This is done by application of the `inactive` class to the element.
@@ -36,3 +36,14 @@ const displayArticles = function({attributes}){
   });
 };
 k.registerFuncs({displayArticles},{type:['opener']});//This function is registered as an opener so that it will be fired every time the sheet is opened. This will ensure that the sheet is displayed always matches the selections. It will also be called in reaction to changes to the `sheet_type` attribute.
+
+
+// tab script
+const buttonlist = ["narrative","tactics","journal","roster","settings"];
+buttonlist.forEach(button => {
+  on(`clicked:${button}`, function() {
+    setAttrs({
+      sheet_state: button
+    });
+  });
+});
