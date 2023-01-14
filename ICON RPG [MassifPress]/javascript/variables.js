823d01655e72923cacf92380d9d0278671d03907
Seraaron <40521082+Seraaron@users.noreply.github.com>
2023-01-14 19:26:54 +0000

    v0.4.2
    forgot to update the current version variable

--- ICON RPG [MassifPress]/javascript/variables.js
+++ ICON RPG [MassifPress]/javascript/variables.js
@@ -3,4 +3,4 @@
 //Sheet Variables
 //const actionAttributes = ['body','mind','spirit','initiative','repeating_skill_$x_roll','repeating_weapon_$x_roll','situational_awareness','remnant_initiative','assault_roll','strike_roll','motion_roll','repeating_remnant-weapon_$x_roll','repeating_drone_$x_assault','repeating_drone_$x_strike'];
 k.sheetName = 'icon';
-k.version = 0.1;
+k.version = 0.4;
