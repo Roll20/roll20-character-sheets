d7113fc27ad4960089ad7293df8ba8f444d8d881
Peter Bjerg Lidegaard <dbf.black.bull@gmail.com>
2023-09-14 19:49:02 +0200

    Fixed both hold person spells

--- ADnD_2E_Revised/javascript/priestSpells.js
+++ ADnD_2E_Revised/javascript/priestSpells.js
@@ -732,17 +732,19 @@ pri2['Hold Person'] = {
     'sphere-spells-and-magic': 'Charm, Law',
     'range': '120 yards',
     'duration': '[[2*[[@{level-priest}]] ]] rounds',
-    'aoe': '[[1d4]] persons in a 20-foot cube',
+    'aoe': '1 to 4 persons in a 20-foot cube',
     'components': 'V, S, M',
     'cast-time': '5',
     'saving-throw': 'Negate',
-    'materials': 'A small, straight piece of iron as the material component of this spell.',
+    'materials': 'A small, straight piece of iron.',
     'reference': 'p. 261',
     'book': 'PHB',
+    'book-compendium': 'Priest’s Spell Compendium Volume Two p. 334',
+    'errata': 'Sage Advice #152 p. 14\nSage Advice #223 p. 79',
     'damage': '',
     'damage-type': '',
     'healing': '',
-    'effect': 'This spell holds 1d4 humans, demihumans, or humanoid creatures rigidly immobile and in place for a minimum of six rounds (the spell lasts 2 rounds per caster level, and the priest must be of at least 3rd level to cast the spell).\n&emsp;The *hold person* spell affects any bipedal human, demihuman, or humanoid of man size or smaller, including brownies, dryads, dwarves, elves, gnolls, gnomes, goblins, half-elves, halflings, half-orcs, hobgoblins, humans, kobolds, lizard men, nixies, orcs, pixies, sprites, troglodytes, and others. Thus, a 10th-level fighter could be held, while an ogre could not.\n&emsp;The effect is centered on a point selected by the caster, and it affects persons selected by the caster within the area of effect. If the spell is cast at three persons, each gets a normal saving throw; if only two persons are being enspelled, each rolls his saving throw with a –1 penalty; if the spell is cast at only one person, the saving throw die roll suffers a –2 penalty. Saving throws are adjusted for Wisdom. Those who succeed on their saving throws are totally unaffected by the spell. Undead creatures cannot be held.\n&emsp;Held creatures cannot move or speak, but they remain aware of events around them and can use abilities not requiring motion or speech. Being held does not prevent the worsening of the subjects’ condition due to wounds, disease, or poison. The priest casting the *hold person* spell can end the spell with a single utterance at any time; otherwise, the duration is six rounds at 3rd level, eight rounds at 4th level, etc.'
+    'effect': 'This spell holds 1 to 4 humans, demihumans, or humanoid creatures rigidly immobile and in place for a minimum of six rounds. The spell lasts two rounds per caster level, and the priest must be of at least 3rd level to cast the spell.\n&emsp;The *hold person* spell affects any bipedal human, demihuman, or humanoid of man size or smaller, including brownies, dryads, dwarves, elves, gnolls, gnomes, goblins, half-elves, halflings, half-orcs, hobgoblins, humans, kobolds, lizard men, nixies, orcs, pixies, sprites, troglodytes, and others. Thus, a 10th-level fighter could be held, while an ogre could not. Undead creatures cannot be held.\n&emsp;The effect is centered on a point selected by the caster, and it affects persons selected by the caster within the area of effect. If the spell is cast at three persons, each gets a normal saving throw; if only two persons are being enspelled, each rolls his saving throw with a –1 penalty; if the spell is cast at but one person, the saving throw die roll suffers a –2 penalty. Saving throws are adjusted for Wisdom. Those who succeed on their saving throws are totally unaffected by the spell.\n&emsp;Held creatures cannot move or speak, but they remain aware of events around them and can use abilities not requiring motion or speech. Being held does not prevent the worsening of the subject’s condition due to wounds, disease, or poison. The priest casting the *hold person* spell can end the spell with a single utterance at any time; otherwise, the duration is six rounds at 3rd level, eight rounds at 4th level, etc.'
 };
 
 pri2['Know Alignment'] = {
