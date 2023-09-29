34ad998c2d3e8f6ecd04951b3837c2b9776fabc3
Daniel Vu <daniel.vu@mailbox.org>
2023-09-29 18:14:57 -0400

    Uses Bun to install latest version of fd-find for required feature

--- update.sh
+++ update.sh
@@ -5,4 +5,4 @@ touch sync_sheets.sh
 echo '#!/bin/bash' > sync_sheets.sh
 echo 'set -e' >> sync_sheets.sh
 chmod +x sync_sheets.sh
-fdfind dist --type d --strip-cwd-prefix -j1 --exec bash -c 'echo ./gcp.sh \"$(printf "%s" "{//}")\" \"$(printf "gs://roll20-cdn/$CDN_SHEETS_FOLDER/%s\n" "{//}")\" \"$(printf "%s" "{//}")/dist\" \"$(printf "%s" "{//}")/translations\"' | tee -a sync_sheets.sh
+fd dist --type d --strip-cwd-prefix -j1 --exec bash -c 'echo ./gcp.sh \"$(printf "%s" "{//}")\" \"$(printf "gs://roll20-cdn/$CDN_SHEETS_FOLDER/%s\n" "{//}")\" \"$(printf "%s" "{//}")/dist\" \"$(printf "%s" "{//}")/translations\"' | tee -a sync_sheets.sh
