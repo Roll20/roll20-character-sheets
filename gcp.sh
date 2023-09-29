391b543e4e36fbc90d8bde7d7901191933855822
Daniel Vu <daniel.vu@mailbox.org>
2023-09-29 18:45:46 -0400

    Removes unneeded step

--- gcp.sh
+++ gcp.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # You need GCloud Access to the dropbox to run this script
 
-gcloud storage rsync --project=roll20-actual "$1" "$2" --delete-unmatched-destination-objects --cache-control='no-cache' --exclude=".*\.css$|.*\.html$|.*\.md$|.*\.sh$|TODO$"
 gcloud storage rsync --project=roll20-actual "$1" "$3" --delete-unmatched-destination-objects --cache-control='no-cache'
 gcloud storage rsync --project=roll20-actual "$1" "$4" --delete-unmatched-destination-objects --cache-control='no-cache'
