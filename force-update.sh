e69bc2605665521db6eb11665b0031c4f5a1b059
Daniel Vu <daniel.vu@mailbox.org>
2023-09-29 17:08:15 -0400

    Adds gcloud CLI setup

--- force-update.sh
+++ force-update.sh
@@ -3,6 +3,7 @@
 rm -rf sync_sheets.sh
 touch sync_sheets.sh
 echo '#!/bin/bash' > sync_sheets.sh
+echo 'set -e' >> sync_sheets.sh
 chmod +x sync_sheets.sh
 fd sheet -e json --strip-cwd-prefix -j1 --exec bash -c 'echo ./gcp.sh \"$(printf "%s" "{//}")\" \"$(printf "gs://roll20-cdn/$CDN_SHEETS_FOLDER/%s\n" "{//}")\" \"$(printf "%s" "{//}")/dist\" \"$(printf "%s" "{//}")/translations\"' | tee -a sync_sheets.sh
 
