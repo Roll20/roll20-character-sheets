6258589716db75393cec3f8fa7f454bc83eafc08
Daniel Vu <daniel.vu@mailbox.org>
2023-10-01 13:03:04 -0400

    Adds force-update mechanism (to upload all sheets)

--- force-update.sh
+++ force-update.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-rm -rf sync_sheets.sh
-touch sync_sheets.sh
-echo '#!/bin/bash' > sync_sheets.sh
-echo 'set -e' >> sync_sheets.sh
-chmod +x sync_sheets.sh
-fd sheet -e json --strip-cwd-prefix -j1 --exec bash -c 'echo ./gcp.sh \"$(printf "%s" "{//}")\" \"$(printf "gs://roll20-cdn/$CDN_SHEETS_FOLDER/%s\n" "{//}")\" \"$(printf "%s" "{//}")/dist\" \"$(printf "%s" "{//}")/translations\"' | tee -a sync_sheets.sh
-
+gcloud storage rsync --project=roll20-actual gs://roll20-cdn/$CDN_SHEETS_FOLDER --delete-unmatched-destination-objects --cache-control='no-cache'
