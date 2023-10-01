a7d3d3296428db09be38270449167c9e2d6a8f19
Daniel Vu <daniel.vu@mailbox.org>
2023-10-01 17:13:39 -0400

    Removes deprecated files

--- release-ci.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# You need GCloud Access to the dropbox to run this script
-
-CURRENT_DIR=$(printf '%s\n' "${PWD##*/}")
-
-gcloud storage rsync --project=roll20-actual ./dist "gs://roll20-cdn/$CDN_SHEETS_FOLDER/$CURRENT_DIR/dist" --delete-unmatched-destination-objects --cache-control='no-cache'
-gcloud storage rsync --project=roll20-actual ./translations "gs://roll20-cdn/$CDN_SHEETS_FOLDER/$CURRENT_DIR/translations" --delete-unmatched-destination-objects --cache-control='no-cache' || true
-
