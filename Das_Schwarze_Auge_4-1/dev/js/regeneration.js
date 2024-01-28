2011f0eb95502cec592deecd9f05525ac1127b32
Kreuvf <webmaster@kreuvf.de>
2024-01-28 20:17:56 +0100

    * Add new advantages/disadvantages, related to regeneration * Add sheetworker for activating/deactivating new coupled advantages/disadvantages

---
+++ Das_Schwarze_Auge_4-1/dev/js/regeneration.js
@@ -0,0 +1,245 @@
+/* regeneration begin */
+/* handle activation/deactivation of advantages/disadvantages */
+on(
+	"change:nachteil_schlechte_regeneration " +
+	"change:vorteil_schnelle_heilung_i " +
+	"change:vorteil_schnelle_heilung_ii " +
+	"change:vorteil_schnelle_heilung_iii ",
+	function(eventInfo) {
+		var attrsToChange = {};
+		var regenerationLevel = 0; // -1 to +3
+		const source = eventInfo.sourceAttribute;
+		const sourceType = eventInfo.sourceType;
+
+		if (sourceType === "player") {
+			// Handle changes to degree of life regeneration
+			if (source === "nachteil_schlechte_regeneration" && eventInfo.newValue === "1") {
+				regenerationLevel = -1;
+			} else if (source === "nachteil_schlechte_regeneration" && eventInfo.newValue === "0") {
+				regenerationLevel = 0;
+			} else if (source === "vorteil_schnelle_heilung_i" && eventInfo.newValue === "1") {
+				regenerationLevel = 1;
+			} else if (source === "vorteil_schnelle_heilung_i" && eventInfo.newValue === "0") {
+				regenerationLevel = 0;
+			} else if (source === "vorteil_schnelle_heilung_ii" && eventInfo.newValue === "1") {
+				regenerationLevel = 2;
+			} else if (source === "vorteil_schnelle_heilung_ii" && eventInfo.newValue === "0") {
+				regenerationLevel = 1;
+			} else if (source === "vorteil_schnelle_heilung_iii" && eventInfo.newValue === "1") {
+				regenerationLevel = 3;
+			} else if (source === "vorteil_schnelle_heilung_iii" && eventInfo.newValue === "0") {
+				regenerationLevel = 2;
+			}
+
+			switch(regenerationLevel) {
+				case -1:
+					attrsToChange["nachteil_schlechte_regeneration"] = "1";
+					attrsToChange["vorteil_schnelle_heilung_i"] = "0";
+					attrsToChange["vorteil_schnelle_heilung_ii"] = "0";
+					attrsToChange["vorteil_schnelle_heilung_iii"] = "0";
+					break;
+				case 0:
+					attrsToChange["nachteil_schlechte_regeneration"] = "0";
+					attrsToChange["vorteil_schnelle_heilung_i"] = "0";
+					attrsToChange["vorteil_schnelle_heilung_ii"] = "0";
+					attrsToChange["vorteil_schnelle_heilung_iii"] = "0";
+					break;
+				case 1:
+					attrsToChange["nachteil_schlechte_regeneration"] = "0";
+					attrsToChange["vorteil_schnelle_heilung_i"] = "1";
+					attrsToChange["vorteil_schnelle_heilung_ii"] = "0";
+					attrsToChange["vorteil_schnelle_heilung_iii"] = "0";
+					break;
+				case 2:
+					attrsToChange["nachteil_schlechte_regeneration"] = "0";
+					attrsToChange["vorteil_schnelle_heilung_i"] = "1";
+					attrsToChange["vorteil_schnelle_heilung_ii"] = "1";
+					attrsToChange["vorteil_schnelle_heilung_iii"] = "0";
+					break;
+				case 3:
+					attrsToChange["nachteil_schlechte_regeneration"] = "0";
+					attrsToChange["vorteil_schnelle_heilung_i"] = "1";
+					attrsToChange["vorteil_schnelle_heilung_ii"] = "1";
+					attrsToChange["vorteil_schnelle_heilung_iii"] = "1";
+					break;
+			}
+
+			// Regeneration mod from advantages/disadvantages
+			attrsToChange["reg_le_mod_vn"] = regenerationLevel; // regeneration, life energy, modifier, advantages/disadvantages (Vorteile/Nachteile)
+			safeSetAttrs(attrsToChange);
+		}
+});
+
+on(
+	"change:nachteil_astraler_block " +
+	"change:vorteil_astrale_regeneration_i " +
+	"change:vorteil_astrale_regeneration_ii " +
+	"change:vorteil_astrale_regeneration_iii ",
+	function(eventInfo) {
+		var attrsToChange = {};
+		var regenerationLevel = 0; // -1 to +3
+		const sourceType = eventInfo.sourceType;
+
+		if (sourceType === "player") {
+			// Handle changes to degree of astral regeneration
+			if (eventInfo.sourceAttribute === "nachteil_astraler_block" && eventInfo.newValue === "1") {
+				regenerationLevel = -1;
+			} else if (eventInfo.sourceAttribute === "nachteil_astraler_block" && eventInfo.newValue === "0") {
+				regenerationLevel = 0;
+			} else if (eventInfo.sourceAttribute === "vorteil_astrale_regeneration_i" && eventInfo.newValue === "1") {
+				regenerationLevel = 1;
+			} else if (eventInfo.sourceAttribute === "vorteil_astrale_regeneration_i" && eventInfo.newValue === "0") {
+				regenerationLevel = 0;
+			} else if (eventInfo.sourceAttribute === "vorteil_astrale_regeneration_ii" && eventInfo.newValue === "1") {
+				regenerationLevel = 2;
+			} else if (eventInfo.sourceAttribute === "vorteil_astrale_regeneration_ii" && eventInfo.newValue === "0") {
+				regenerationLevel = 1;
+			} else if (eventInfo.sourceAttribute === "vorteil_astrale_regeneration_iii" && eventInfo.newValue === "1") {
+				regenerationLevel = 3;
+			} else if (eventInfo.sourceAttribute === "vorteil_astrale_regeneration_iii" && eventInfo.newValue === "0") {
+				regenerationLevel = 2;
+			}
+
+			// Populate attrsToChange so we don't forget later on
+			switch(regenerationLevel) {
+				case -1:
+					attrsToChange["nachteil_astraler_block"] = "1";
+					attrsToChange["vorteil_astrale_regeneration_i"] = "0";
+					attrsToChange["vorteil_astrale_regeneration_ii"] = "0";
+					attrsToChange["vorteil_astrale_regeneration_iii"] = "0";
+					attrsToChange["reg_ae_in"] = "@{IN} - (1d20 + 2)";
+					break;
+				case 0:
+					attrsToChange["nachteil_astraler_block"] = "0";
+					attrsToChange["vorteil_astrale_regeneration_i"] = "0";
+					attrsToChange["vorteil_astrale_regeneration_ii"] = "0";
+					attrsToChange["vorteil_astrale_regeneration_iii"] = "0";
+					attrsToChange["reg_ae_in"] = "@{IN} - 1d20";
+					break;
+				case 1:
+					attrsToChange["nachteil_astraler_block"] = "0";
+					attrsToChange["vorteil_astrale_regeneration_i"] = "1";
+					attrsToChange["vorteil_astrale_regeneration_ii"] = "0";
+					attrsToChange["vorteil_astrale_regeneration_iii"] = "0";
+					attrsToChange["reg_ae_in"] = "@{IN} - (1d20 - 1)";
+					break;
+				case 2:
+					attrsToChange["nachteil_astraler_block"] = "0";
+					attrsToChange["vorteil_astrale_regeneration_i"] = "1";
+					attrsToChange["vorteil_astrale_regeneration_ii"] = "1";
+					attrsToChange["vorteil_astrale_regeneration_iii"] = "0";
+					attrsToChange["reg_ae_in"] = "@{IN} - (1d20 - 2)";
+					break;
+				case 3:
+					attrsToChange["nachteil_astraler_block"] = "0";
+					attrsToChange["vorteil_astrale_regeneration_i"] = "1";
+					attrsToChange["vorteil_astrale_regeneration_ii"] = "1";
+					attrsToChange["vorteil_astrale_regeneration_iii"] = "1";
+					attrsToChange["reg_ae_in"] = "@{IN} - (1d20 - 3)";
+					break;
+			}
+
+			// Regeneration mod from advantages/disadvantages
+			attrsToChange["reg_ae_mod_vn"] = regenerationLevel;	// regeneration, astral energy, modifier, advantages/disadvantages (Vorteile/Nachteile)
+			safeSetAttrs(attrsToChange);
+		}
+});
+
+on(
+	"change:nachteil_krankheitsanfaellig " +
+	"change:vorteil_immunitaet_gegen_krankheiten_alle " +
+	"change:vorteil_immunitaet_gegen_krankheiten_wundfieber " +
+	"change:vorteil_resistenz_gegen_krankheiten_alle ",
+	function(eventInfo) {
+		var attrsToChange = {};
+		const sourceType = eventInfo.sourceType;
+
+		if (sourceType === "player") {
+			// Handle changes to degree of astral regeneration
+			if (eventInfo.sourceAttribute === "nachteil_krankheitsanfaellig" && eventInfo.newValue === "1") {
+				attrsToChange["vorteil_immunitaet_gegen_krankheiten_alle"] = "0";
+				attrsToChange["vorteil_immunitaet_gegen_krankheiten_wundfieber"] = "0";
+				attrsToChange["vorteil_resistenz_gegen_krankheiten_alle"] = "0";
+			} else if (eventInfo.sourceAttribute === "vorteil_resistenz_gegen_krankheiten_alle" && eventInfo.newValue === "1") {
+				attrsToChange["vorteil_immunitaet_gegen_krankheiten_alle"] = "0";
+				// immunity against a specific disease possible with resistence against all diseases
+				attrsToChange["nachteil_krankheitsanfaellig"] = "0";
+			} else if (eventInfo.sourceAttribute === "vorteil_immunitaet_gegen_krankheiten_wundfieber" && eventInfo.newValue === "1") {
+				attrsToChange["nachteil_krankheitsanfaellig"] = "0";
+			} else if (eventInfo.sourceAttribute === "vorteil_immunitaet_gegen_krankheiten_alle" && eventInfo.newValue === "1") {
+				attrsToChange["vorteil_immunitaet_gegen_krankheiten_wundfieber"] = "0";
+				attrsToChange["vorteil_resistenz_gegen_krankheiten_alle"] = "0";
+				attrsToChange["nachteil_krankheitsanfaellig"] = "0";
+			}
+			safeSetAttrs(attrsToChange);
+		}
+});
+
+on(
+	"change:nachteil_schlafstoerungen_i " +
+	"change:nachteil_schlafstoerungen_ii ",
+	function(eventInfo) {
+		var attrsToChange = {};
+		const sourceType = eventInfo.sourceType;
+
+		if (sourceType === "player") {
+			// Handle changes to degree of astral regeneration
+			// No matter whether it's "0" or "1", deactivate the second level
+			if (eventInfo.sourceAttribute === "nachteil_schlafstoerungen_i") {
+				attrsToChange["nachteil_schlafstoerungen_ii"] = "0";
+			} else if (eventInfo.sourceAttribute === "nachteil_schlafstoerungen_ii" && eventInfo.newValue === "1") {
+				attrsToChange["nachteil_schlafstoerungen_i"] = "1";
+			}
+
+			safeSetAttrs(attrsToChange);
+		}
+});
+
+/* handle regeneration-related values */
+on(
+	"change:reg_le_mod_vn " +
+	"change:reg_ae_mod_vn " +
+	"change:nachteil_heimwehkrank " +
+	"change:nachteil_nahrungsrestriktion " +
+	"change:nachteil_schlafstoerungen_i " +
+	"change:nachteil_schlafstoerungen_ii " +
+	"change:nachteil_schlafwandler " +
+	"change:nachteil_sucht " +
+	"change:nachteil_sucht_giftstufe " +
+	"change:nachteil_verwoehnt " +
+	"change:nachteil_verwoehnt_wert ",
+	function() {
+	safeGetAttrs([
+		"reg_le_mod_vn",
+		"reg_ae_mod_vn",
+		"nachteil_heimwehkrank",
+		"nachteil_nahrungsrestriktion",
+		"nachteil_schlafstoerungen_i",
+		"nachteil_schlafstoerungen_ii",
+		"nachteil_schlafwandler",
+		"nachteil_sucht",
+		"nachteil_sucht_giftstufe",
+		"nachteil_verwoehnt",
+		"nachteil_verwoehnt_wert"
+	], function(v) {
+		/* brauchen noch mehr UI
+		heimwehkrank -> "außerhalb der Heimat" -> ja -> ae reg -1
+		nahrungsrestriktion -> "Tage seit letzter Einhaltung der Restriktion" -> >0 und <=3 -> le/ae reg -2; >3 le/ae reg = 0, KO fällt (wird erst mal nur mitgeteilt, nicht berücksichtigt)
+		schlafstoerungen_i -> 4 auf 1W4 -> le/ae reg nur 1w6-1, schlafdauer wie 4 h, Status "unausgeschlafen" -> AU auf 3/4 des max. cappen, Schlechte Eigenschaften +2, Talent/Zauberproben +3
+		schlafstoerungen_ii -> 2 auf 1W2 -> wie schlafstoerungen_i; sonst: Einschlafprobe (Selbstbeherrschung +7) bei Scheitern wie schlafstoerungen_i
+		schlafwandler -> 4 auf W4 -> le/ae reg -1
+		sucht -> tage seit letztem Konsum -> Wert >= (15 - Giftstufe) -> le/ae reg = 0, pro Tag +Erschöpfung Giftstufe/3, schlechte Eigenschaften +Giftstufe/3, Talent- und Zauberproben +Giftstufe/3; Wert >= 2 * (15 - Giftstufe) -> erschöpfung, schlechte Eigenschaften, Probenmod +Giftstufe/2; Warnung bei Überanstrengung auf Max. -> KO-Senkung nicht implementiert; KO = 0 -> Giftstufe/2 SP pro Tag; vollständige (!?) Wiederherstellung durch eine Dosis möglich (Überanstrengung, Wiederaufbau KO "normal")
+		verwoehnt -> "Schlafstätte angemessen?" nein -> le/ae-reg-bonus aus KO/IN +verwoehnt_wert, bei Scheitern -> -1; 1. Schwelle für BE aus Last bei KK - 2
+		*/
+		/*
+		Wundheilung aufnehmen
+			mehr als 5 SP unbehandelt seit letzter Regeneration? -> Wundfieber möglich (bei gescheiterter oder ausgebliebener HKW-Probe) -> weitere Vor- und Nachteile beachten: Immunität gegen Krankheiten (alle/Wundfieber), Resistenz gegen Krankheiten (Wechselwirkung mit "Heimwehkrank" berücksichtigen), Krankheitsanfällig
+			Abfrage im Roll: Erstversorgung oder Nachbehandlung -> TaP* entsprechend verrechnen
+			Reihenfolge für Wundheilung festlegen: Kopf, Brust/Rücken, Bauch, RA, LA, RB, LB
+			Schlechte Regeneration: +2 auf Wundheil-KO!
+		*/
+		/* Astrale Meditation: Thonnys mit/ohne SF */
+		//safeSetAttrs(attrsToChange);
+	});
+});
+/* regeneration end */
