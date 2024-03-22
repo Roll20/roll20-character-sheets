48ef5109fa404e54d538588ad1823021a3cae566
uriele <menarini.marco@gmail.com>
2024-03-22 15:02:09 +0100

    working with named button
    modified the button CSS to allow named buttons using span

--- Delta Green 2/src/pug/dist/sheetworkers.dev.js
+++ Delta Green 2/src/pug/dist/sheetworkers.dev.js
@@ -1,10 +1,10 @@
 "use strict";
 
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
@@ -450,21 +450,284 @@ var isJSONString = function isJSONString(string) {
   return json;
 };
 
-var dropWeapon = function dropWeapon(data) {
+var dropAgent = function dropAgent(data) {
   var updateAttrs = {};
-  var fields = ["weapons", "skill_percent", "base_range", "damage", "armor_piercing", "lethality_percent", "ammo"];
-  var UIDD = generateRowID();
-  var prefix = "repeating_weapons_".concat(UIDD);
+  updateAttrs["sheet_type"] = "pc";
+  var fields = [// general information
+  "employer", "profession", //skills
+  "accounting", "alertness", "anthropology", "archeology", "art", "art_name", "artillery", "athletics", "bureaucracy", "computer_science", "craft", "craft_name", "criminology", "demolitions", "disguise", "dodge", "drive", "firearms", "first_aid", "forensics", "heavy_machinery", "heavy_weapons", "history", "humint", "law", "medicine", "melee_weapons", "military_science", "military_science_name", "navigate", "occult", "persuade", "pharmacy", "pilot", "pilot_name", "psychotherapy", "ride", "science", "science_name", "search", "sigint", "stealth", "surgery", "survival", "swim", "unarmed_combat", "unnatural", //additional entries
+  "armor_and_gear", "personal_details_and_notes", "motivations", "bond_number"]; // bonds
 
-  for (var field in fields) {
-    if (data["repeating_weapons_".concat(field)]) {
-      updateAttrs["".concat(prefix, "_").concat(field)] = data["".concat(prefix, "_").concat(field)];
+  var special_trainings = isJSONString(data.repeated_special_trainings);
+  var rituals = isJSONString(data.repeating_rituals);
+  var weapons = isJSONString(data.repeating_weapons);
+  var repSkills = isJSONString(data.repeating_skills);
+  console.log("special_trainings: ".concat(special_trainings));
+  console.log("rituals: ".concat(rituals));
+  console.log("weapons: ".concat(weapons));
+  console.log("repSkills: ".concat(repSkills));
+
+  if (repSkills) {
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+      var _loop = function _loop() {
+        var skill = _step.value;
+        var dRepSkills = dropRepSkills(skill);
+        Object.keys(dRepSkills).forEach(function (key) {
+          return updateAttrs[key] = dRepSkills[key];
+        });
+      };
+
+      for (var _iterator = repSkills[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        _loop();
+      }
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+          _iterator["return"]();
+        }
+      } finally {
+        if (_didIteratorError) {
+          throw _iteratorError;
+        }
+      }
+    }
+  }
+
+  if (special_trainings) {
+    var _iteratorNormalCompletion2 = true;
+    var _didIteratorError2 = false;
+    var _iteratorError2 = undefined;
+
+    try {
+      var _loop2 = function _loop2() {
+        var special_training = _step2.value;
+        var dSpecialTraining = dropSpecialTraining(special_training);
+        Object.keys(dSpecialTraining).forEach(function (key) {
+          return updateAttrs[key] = dSpecialTraining[key];
+        });
+      };
+
+      for (var _iterator2 = special_trainings[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+        _loop2();
+      }
+    } catch (err) {
+      _didIteratorError2 = true;
+      _iteratorError2 = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion2 && _iterator2["return"] != null) {
+          _iterator2["return"]();
+        }
+      } finally {
+        if (_didIteratorError2) {
+          throw _iteratorError2;
+        }
+      }
+    }
+  }
+
+  if (rituals) {
+    var _iteratorNormalCompletion3 = true;
+    var _didIteratorError3 = false;
+    var _iteratorError3 = undefined;
+
+    try {
+      var _loop3 = function _loop3() {
+        var ritual = _step3.value;
+        var dRitual = dropRitual(ritual);
+        Object.keys(dRitual).forEach(function (key) {
+          return updateAttrs[key] = dRitual[key];
+        });
+      };
+
+      for (var _iterator3 = rituals[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+        _loop3();
+      }
+    } catch (err) {
+      _didIteratorError3 = true;
+      _iteratorError3 = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion3 && _iterator3["return"] != null) {
+          _iterator3["return"]();
+        }
+      } finally {
+        if (_didIteratorError3) {
+          throw _iteratorError3;
+        }
+      }
+    }
+  }
+
+  if (weapons) {
+    var _iteratorNormalCompletion4 = true;
+    var _didIteratorError4 = false;
+    var _iteratorError4 = undefined;
+
+    try {
+      var _loop4 = function _loop4() {
+        var weapon = _step4.value;
+        var dWeapons = dropWeapon(weapon);
+        Object.keys(dWeapons).forEach(function (key) {
+          return updateAttrs[key] = dWeapons[key];
+        });
+      };
+
+      for (var _iterator4 = weapons[Symbol.iterator](), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
+        _loop4();
+      }
+    } catch (err) {
+      _didIteratorError4 = true;
+      _iteratorError4 = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion4 && _iterator4["return"] != null) {
+          _iterator4["return"]();
+        }
+      } finally {
+        if (_didIteratorError4) {
+          throw _iteratorError4;
+        }
+      }
+    }
+  }
+
+  console.log(fields);
+
+  for (var _i = 0, _fields = fields; _i < _fields.length; _i++) {
+    var field = _fields[_i];
+    console.log(field);
+    console.log(data[field]);
+
+    if (data[field]) {
+      if (field === "bond_number") {
+        for (i = 0; i < data[field]; i++) {
+          var UIDD = generateRowID();
+          var prefix = "repeating_bonds_".concat(UIDD);
+          updateAttrs["".concat(prefix, "_name")] = "bond_".concat(i);
+        }
+      } else {
+        updateAttrs[field] = data[field];
+      }
     }
   }
 
   console.log(updateAttrs);
   return updateAttrs;
 };
+
+var dropRepSkills = function dropRepSkills(data) {
+  var updateAttrs = {};
+  var UIDD = generateRowID();
+  var prefix = "repeating_skills_".concat(UIDD);
+  updateAttrs["".concat(prefix, "_name")] = data.name;
+  updateAttrs["".concat(prefix, "_rank")] = data.rank;
+  console.log("skills");
+  console.log(updateAttrs);
+  return updateAttrs;
+};
+
+var dropSpecialTraining = function dropSpecialTraining(data) {
+  var updateAttrs = {};
+  var UIDD = generateRowID();
+  var prefix = "repeating_special_".concat(UIDD);
+  fields = ["special_training", "skill_or_stat_used"];
+  var _iteratorNormalCompletion5 = true;
+  var _didIteratorError5 = false;
+  var _iteratorError5 = undefined;
+
+  try {
+    for (var _iterator5 = fields[Symbol.iterator](), _step5; !(_iteratorNormalCompletion5 = (_step5 = _iterator5.next()).done); _iteratorNormalCompletion5 = true) {
+      var field = _step5.value;
+
+      if (data[field]) {
+        if (field === "skill_or_stat_used") {
+          var skillname = data["skill_or_stat_used"].toLowerCase().replace(/ /g, "_");
+          updateAttrs["".concat(prefix, "_skill_or_stat_used")] = "@{".concat(skillname, "}");
+        } else {
+          updateAttrs["".concat(prefix, "_").concat(field)] = data[field];
+        }
+      }
+    }
+  } catch (err) {
+    _didIteratorError5 = true;
+    _iteratorError5 = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion5 && _iterator5["return"] != null) {
+        _iterator5["return"]();
+      }
+    } finally {
+      if (_didIteratorError5) {
+        throw _iteratorError5;
+      }
+    }
+  }
+
+  console.log("special training");
+  console.log(updateAttrs);
+  return updateAttrs;
+};
+
+var dropRitual = function dropRitual(data) {
+  var updateAttrs = {};
+  var UIDD = generateRowID();
+  var prefix = "repeating_rituals_".concat(UIDD); // to be updated when the format is decided
+
+  console.log(updateAttrs);
+  return updateAttrs;
+};
+
+var dropMonster = function dropMonster(data) {
+  var updateAttrs = {};
+  updateAttrs["sheet_type"] = "npc"; // to be updated when the format is decided
+
+  console.log(updateAttrs);
+  return updateAttrs;
+};
+
+var dropWeapon = function dropWeapon(data) {
+  var updateAttrs = {};
+  var fields = ["weapons", "skill_percent", "base_range", "damage", "armor_piercing", "lethality_percent", "ammo"];
+  var UIDD = generateRowID();
+  var prefix = "repeating_weapons_".concat(UIDD);
+
+  for (var _i2 = 0, _fields2 = fields; _i2 < _fields2.length; _i2++) {
+    var field = _fields2[_i2];
+
+    if (data[field]) {
+      if (field === "skill_percent") {
+        var skillname = data[field].toLowerCase().replace(/ /g, "_");
+        updateAttrs["".concat(prefix, "_").concat(field)] = "@{".concat(skillname, "}");
+      } else {
+        updateAttrs["".concat(prefix, "_").concat(field)] = data[field];
+      }
+    }
+  }
+
+  console.log(updateAttrs);
+  return updateAttrs;
+};
+/*
+const dropWeaponWrapper = (data) => {
+	const updateAttrs = {};
+	const weapon = isJSONString(data.repeating_weapons);
+	let dWeapons=dropWeapon(weapon);
+	Object.keys(dWeapons).forEach(key => updateAttrs[key] = dWeapons[key]);
+	console.log("here the new weapon");
+	console.log(updateAttrs);
+	return updateAttrs;
+
+}
+*/
+
 
 var getDropType = function getDropType(data) {
   return data.Category || false;
@@ -483,10 +746,58 @@ var handleDragandDrop = function handleDragandDrop() {
     var dropType = getDropType(dropDataParsed);
     console.log(dropType);
     console.log(dropDataParsed);
-    var updateAttrs = dropType === "Weapons" ? dropWeapon(dropDataParsed["repeating_weapons"]) : false;
-    console.log(updateAttrs);
-    if (updateAttrs === false) return console.warn("Drag and drop could not identify the type of drop");
-    if (_typeof(updateAttrs) !== "object") return console.warn("Drag and drop returned  broken value");
+    var updateAttrs = false;
+
+    if (dropType === "Agents") {
+      console.log("It's an Agent");
+      updateAttrs = dropAgent(dropDataParsed);
+    }
+
+    if (dropType === "Monsters") {
+      console.log("It's a Monster");
+      updateAttrs = dropMonster(dropDataParsed);
+    }
+
+    if (dropType === "Rituals") {
+      console.log("It's a ritual");
+      updateAttrs = dropRitual(dropDataParsed);
+    }
+
+    if (dropType === "Bonds") {
+      console.log("It's a Bond");
+      updateAttrs = dropBond(dropDataParsed);
+    }
+
+    if (dropType === "SpecialTrainings") {
+      console.log("It's an Special Training");
+      updateAttrs = dropSpecialTraining(dropDataParsed);
+    }
+
+    if (dropType === "Weapons") {
+      console.log("It's a Weapon");
+      updateAttrs = dropWeapon(dropDataParsed);
+    }
+
+    if (updateAttrs === false) {
+      updateAttrs = _objectSpread({}, {
+        drop_name: "",
+        drop_data: "",
+        drop_category: ""
+      });
+      setAttrs(updateAttrs);
+      return console.warn("Drag and drop could not identify the type of drop");
+    }
+
+    if (_typeof(updateAttrs) !== "object") {
+      updateAttrs = _objectSpread({}, {
+        drop_name: "",
+        drop_data: "",
+        drop_category: ""
+      });
+      setAttrs(updateAttrs);
+      return console.warn("Drag and drop returned  broken value");
+    }
+
     updateAttrs = _objectSpread({}, updateAttrs, {}, {
       drop_name: "",
       drop_data: "",
@@ -497,7 +808,8 @@ var handleDragandDrop = function handleDragandDrop() {
   });
 };
 
-on("sheet:compendium-drop", function (eventInfo) {
+on("change:drop_data", function (eventInfo) {
   var jsonData = JSON.parse(eventInfo.newValue);
-  console.log(jsonData); // do something with data
+  console.log(jsonData);
+  handleDragandDrop(); // do something with data
 });
--- Delta Green 2/src/pug/sheetworkers.js
+++ Delta Green 2/src/pug/sheetworkers.js
@@ -411,20 +411,208 @@ const isJSONString = (string) => {
 	return json;
 }
 
+const dropAgent = (data) => {
+	const updateAttrs = {};
+	updateAttrs[`sheet_type`] = "pc";
+	const fields = [
+		// general information
+		"employer",
+		"profession",
+		//skills
+		"accounting",
+		"alertness",
+		"anthropology",
+		"archeology",
+		"art",
+		"art_name",
+		"artillery",
+		"athletics",
+		"bureaucracy",
+		"computer_science",
+		"craft",
+		"craft_name",
+		"criminology",
+		"demolitions",
+		"disguise",
+		"dodge",
+		"drive",
+		"firearms",
+		"first_aid",
+		"forensics",
+		"heavy_machinery",
+		"heavy_weapons",
+		"history",
+		"humint",
+		"law",
+		"medicine",
+		"melee_weapons",
+		"military_science",
+		"military_science_name",
+		"navigate",
+		"occult",
+		"persuade",
+		"pharmacy",
+		"pilot",
+		"pilot_name",
+		"psychotherapy",
+		"ride",
+		"science",
+		"science_name",
+		"search",
+		"sigint",
+		"stealth",
+		"surgery",
+		"survival",
+		"swim",
+		"unarmed_combat",
+		"unnatural",
+		//additional entries
+		"armor_and_gear",
+		"personal_details_and_notes",
+		"motivations",
+		"bond_number"
+	];
+
+	// bonds
+	const special_trainings = isJSONString(data.repeated_special_trainings);
+	const rituals= isJSONString(data.repeating_rituals);
+	const weapons= isJSONString(data.repeating_weapons);
+	const repSkills= isJSONString(data.repeating_skills);
+	console.log(`special_trainings: ${special_trainings}`);
+	console.log(`rituals: ${rituals}`);
+	console.log(`weapons: ${weapons}`);
+	console.log(`repSkills: ${repSkills}`);
+
+	if (repSkills){
+		for (const skill of repSkills){
+			let dRepSkills=dropRepSkills(skill);
+			Object.keys(dRepSkills).forEach(key => updateAttrs[key] = dRepSkills[key]);
+		}
+	}
+
+	if (special_trainings){
+		for (const special_training of special_trainings){
+			let dSpecialTraining=dropSpecialTraining(special_training);
+			Object.keys(dSpecialTraining).forEach(key => updateAttrs[key] = dSpecialTraining[key]);
+		}
+	}
+	
+	if (rituals){
+		for (const ritual of rituals){
+			let dRitual=dropRitual(ritual);
+			Object.keys(dRitual).forEach(key => updateAttrs[key] = dRitual[key]);
+		}
+	}
+
+	if (weapons){
+		for (const weapon of weapons){
+			let dWeapons=dropWeapon(weapon);
+			Object.keys(dWeapons).forEach(key => updateAttrs[key] = dWeapons[key]);
+		}
+	}
+
+	console.log(fields);
+	for (const field of fields){
+		console.log(field);
+		console.log(data[field]);
+		if (data[field]){
+			if (field === "bond_number") {	
+				for (i=0; i<data[field]; i++){	
+					const UIDD = generateRowID();
+					const prefix=`repeating_bonds_${UIDD}`;
+					updateAttrs[`${prefix}_name`] = `bond_${i}`;
+				}
+			}else{
+			updateAttrs[field] = data[field];
+			}
+		}
+	}
+
+	console.log(updateAttrs);
+	return updateAttrs;
+}
+
+const dropRepSkills = (data) => {
+	const updateAttrs = {};
+	const UIDD = generateRowID();
+	const prefix=`repeating_skills_${UIDD}`;
+	updateAttrs[`${prefix}_name`] = data.name;
+	updateAttrs[`${prefix}_rank`] = data.rank;
+	console.log("skills")
+	console.log(updateAttrs);
+	return updateAttrs;
+}
+
+const dropSpecialTraining = (data) => {
+	const updateAttrs = {};
+	const UIDD = generateRowID();
+	const prefix=`repeating_special_${UIDD}`;
+	fields=["special_training","skill_or_stat_used"];
+	for (const field of fields){
+		if (data[field]){ 
+			if (field === "skill_or_stat_used"){		
+				let skillname=data[`skill_or_stat_used`].toLowerCase().replace(/ /g, "_");
+				updateAttrs[`${prefix}_skill_or_stat_used`] = `@{${skillname}}`;
+			}else{
+				updateAttrs[`${prefix}_${field}`] = data[field];
+			}
+		}
+	}
+	console.log("special training");
+	console.log(updateAttrs);
+	return updateAttrs;
+}
+
+
+const dropRitual = (data) => {
+	const updateAttrs = {};
+	const UIDD = generateRowID();
+	const prefix=`repeating_rituals_${UIDD}`;
+	// to be updated when the format is decided
+
+	console.log(updateAttrs);
+	return updateAttrs;
+}
+
+const dropMonster = (data) => {
+	const updateAttrs = {};
+	updateAttrs[`sheet_type`] = "npc";
+	// to be updated when the format is decided
+	console.log(updateAttrs);
+	return updateAttrs;
+}
+
 const dropWeapon = (data) => {
 	const updateAttrs = {};
 	const fields = ["weapons", "skill_percent", "base_range", "damage", "armor_piercing", "lethality_percent", "ammo"];
 	const UIDD = generateRowID();
 	const prefix=`repeating_weapons_${UIDD}`;
-	for (const field in fields){
-		if (data[`repeating_weapons_${field}`]){
-			updateAttrs[`${prefix}_${field}`] = data[`${prefix}_${field}`];
+	for (const field of fields){
+		if (data[field]){
+			if (field === "skill_percent"){
+				let skillname=data[field].toLowerCase().replace(/ /g, "_");
+				updateAttrs[`${prefix}_${field}`] = `@{${skillname}}`;
+			}else{
+				updateAttrs[`${prefix}_${field}`] = data[field];
+			}
 		}
 	}
 	console.log(updateAttrs);
 	return updateAttrs;
 }
 
+/*
+const dropWeaponWrapper = (data) => {
+	const updateAttrs = {};
+	const weapon = isJSONString(data.repeating_weapons);
+	let dWeapons=dropWeapon(weapon);
+	Object.keys(dWeapons).forEach(key => updateAttrs[key] = dWeapons[key]);
+	console.log("here the new weapon");
+	console.log(updateAttrs);
+	return updateAttrs;
+
+}
+*/
 const getDropType = data => data.Category || false;
 
 const handleDragandDrop = () => {
@@ -435,12 +623,41 @@ const handleDragandDrop = () => {
 		const dropType = getDropType(dropDataParsed);
 		console.log(dropType);
 		console.log(dropDataParsed);
-		let updateAttrs = (dropType === "Weapons") ? dropWeapon(dropDataParsed[`repeating_weapons`]) : false;
-		console.log(updateAttrs);
-		if (updateAttrs===false) return	console.warn("Drag and drop could not identify the type of drop");
-
-		if (typeof updateAttrs !== "object") return console.warn("Drag and drop returned  broken value");
-
+		let updateAttrs = false;
+		if (dropType === "Agents"){     
+			console.log("It's an Agent");
+		   updateAttrs=dropAgent(dropDataParsed);
+		}
+		if (dropType === "Monsters"){     
+			console.log("It's a Monster");
+            updateAttrs=dropMonster(dropDataParsed);
+		}
+		if (dropType === "Rituals"){     
+			console.log("It's a ritual");
+	        updateAttrs=dropRitual(dropDataParsed);
+		}
+		if (dropType === "Bonds"){     
+			console.log("It's a Bond");
+            updateAttrs=dropBond(dropDataParsed);
+		}
+		if (dropType === "SpecialTrainings"){     
+			console.log("It's an Special Training");
+			updateAttrs=dropSpecialTraining(dropDataParsed);
+		}
+		if (dropType === "Weapons"){
+	        console.log("It's a Weapon");
+			updateAttrs=dropWeapon(dropDataParsed);
+		}
+		if (updateAttrs === false) {
+			updateAttrs = {...{drop_name:"", drop_data:"",drop_category:""}};
+			setAttrs(updateAttrs);
+			return	console.warn("Drag and drop could not identify the type of drop");
+		}
+		if (typeof updateAttrs !== "object") {
+			updateAttrs = {...{drop_name:"", drop_data:"",drop_category:""}};
+			setAttrs(updateAttrs);
+			return console.warn("Drag and drop returned  broken value");
+		}
 		updateAttrs = {...updateAttrs, ...{drop_name:"", drop_data:"",drop_category:""}};
 		console.log(updateAttrs);
 		setAttrs(updateAttrs);
@@ -448,8 +665,9 @@ const handleDragandDrop = () => {
 	});
 }
 
-on("sheet:compendium-drop", (eventInfo) => {
+on("change:drop_data", (eventInfo) => {
 	const jsonData = JSON.parse(eventInfo.newValue);
 	console.log(jsonData);
+	handleDragandDrop();
 	// do something with data
    });
