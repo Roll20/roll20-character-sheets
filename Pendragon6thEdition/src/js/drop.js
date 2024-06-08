9acc1c94621fe9e9c27898d003d0d460173cfba7
CassieLevett <cassielevett@gmail.com>
2024-06-08 00:34:47 +0000

    add compendium warning to drag n drop

--- Pendragon6thEdition/src/js/drop.js
+++ Pendragon6thEdition/src/js/drop.js
@@ -1,3 +1,44 @@
+const handle_npc = (page) => {
+  console.log(page);
+};
+
+const handle_item = (page) => {
+  console.log(page["Subcategory"]);
+  const subcategory = page["Subcategory"];
+
+  switch (subcategory) {
+    case "Weapon":
+      handle_weapon(page);
+      break;
+    case "Ranged Weapon":
+      handle_ranged_weapon(page);
+      break;
+    case "Animal":
+      handle_animal(page);
+      break;
+    case "Armor":
+      handle_armor(page);
+      break;
+    case "Horse":
+      handle_horse(page);
+      break;
+    case "Horse Armor":
+      handle_horse_armor(page);
+      break;
+    case "Service":
+      handle_service(page);
+      break;
+    case "Shield":
+      handle_shield(page);
+      break;
+    case "Squire":
+      handle_squire(page);
+      break;
+    default:
+      console.log(`Unknown subcategory: ${subcategory}`);
+  }
+};
+
 const handle_drop = (category) => {
   getAttrs(["drop_name", "drop_data", "drop_content"], (v) => {
     let pagedata = v.drop_data;
@@ -13,7 +54,18 @@ const handle_drop = (category) => {
       data: pagedata,
       content: v.drop_content,
     };
-    const category = page.data["Category"];
+
+    switch (category) {
+      case "Npc":
+        handle_npc(page);
+        break;
+      case "Item":
+        handle_item(page);
+        break;
+      default:
+        console.log(`Unknown category: ${category}`);
+    }
+
     // get_repeating_data(function (repeating) {
     //   var results = processDrop(page, v, repeating);
     //   setAttrs(
@@ -32,18 +84,17 @@ const handle_drop = (category) => {
 };
 
 on("change:drop_category", (event) => {
-  console.log(event);
-  handle_drop(eventinfo.newValue);
+  handle_drop(event.newValue);
 });
 
-on("change:drop_name", (event) => {
-  console.log(event);
-});
+// on("change:drop_name", (event) => {
+//   console.log(event);
+// });
 
-on("change:drop_data", (event) => {
-  console.log(event);
-});
+// on("change:drop_data", (event) => {
+//   console.log(event);
+// });
 
-on("change:drop_content", (event) => {
-  console.log(event);
-});
+// on("change:drop_content", (event) => {
+//   console.log(event);
+// });
