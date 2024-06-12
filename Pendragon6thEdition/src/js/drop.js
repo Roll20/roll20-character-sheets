01fec203ea6da2dbfa4256d8914a82c85b5b7078
CassieLevett <cassielevett@gmail.com>
2024-06-12 19:38:09 +0000

    save wip on drag n drop

--- Pendragon6thEdition/src/js/drop.js
+++ Pendragon6thEdition/src/js/drop.js
@@ -1,70 +1,103 @@
+const dropWarning = (v) => {
+  console.log(
+    `%c Pendragon 6th Edition: ${v}`,
+    "color: orange; font-weight:bold"
+  );
+};
+
 const handle_npc = (page) => {
   console.log(page);
 };
 
 const handle_item = (page) => {
   console.log(page["Subcategory"]);
-  const subcategory = page["Subcategory"];
+  // const subcategory = page["Subcategory"];
 
-  switch (subcategory) {
-    case "Weapon":
-      handle_weapon(page);
-      break;
-    case "Ranged Weapon":
-      handle_ranged_weapon(page);
-      break;
-    case "Animal":
-      handle_animal(page);
-      break;
-    case "Armor":
-      handle_armor(page);
-      break;
-    case "Horse":
-      handle_horse(page);
-      break;
-    case "Horse Armor":
-      handle_horse_armor(page);
-      break;
-    case "Service":
-      handle_service(page);
-      break;
-    case "Shield":
-      handle_shield(page);
-      break;
-    case "Squire":
-      handle_squire(page);
-      break;
-    default:
-      console.log(`Unknown subcategory: ${subcategory}`);
-  }
+  // console.log(page);
+
+  // switch (subcategory) {
+  //   case "Weapon":
+  //     handle_weapon(page);
+  //     break;
+  //   case "Ranged Weapon":
+  //     handle_ranged_weapon(page);
+  //     break;
+  //   case "Animal":
+  //     handle_animal(page);
+  //     break;
+  //   case "Armor":
+  //     handle_armor(page);
+  //     break;
+  //   case "Horse":
+  //     handle_horse(page);
+  //     break;
+  //   case "Horse Armor":
+  //     handle_horse_armor(page);
+  //     break;
+  //   case "Service":
+  //     handle_service(page);
+  //     break;
+  //   case "Shield":
+  //     handle_shield(page);
+  //     break;
+  //   case "Squire":
+  //     handle_squire(page);
+  //     break;
+  //   default:
+  //     dropWarning(`Unknown subcategory: ${subcategory}`);
+  // }
 };
 
-const handle_drop = (category) => {
-  getAttrs(["drop_name", "drop_data", "drop_content"], (v) => {
-    let pagedata = v.drop_data;
+const handle_drop = () => {
+  dropWarning("handle_drop");
 
-    try {
-      pagedata = JSON.parse(v.drop_data);
-    } catch (e) {
-      console.log(`Error parsing JSON: ${v.drop_data}`);
+  getAttrs(["drop_name", "drop_data", "drop_content", "drop_category"], (v) => {
+    if (!v.drop_name || !v.drop_data || !v.drop_content || !v.drop_category) {
+      return;
     }
 
-    const page = {
+    // let pagedata = v.drop_data;
+
+    // try {
+    //   pagedata = JSON.parse(v.drop_data);
+    // } catch (e) {
+    //   console.log(`Error parsing JSON: ${v.drop_data}`);
+    // }
+
+    // const page = {
+    //   name: v.drop_name,
+    //   data: pagedata,
+    //   content: v.drop_content,
+    //   category: v.drop_category,
+    // };
+
+    console.table({
+      category: v.drop_category,
       name: v.drop_name,
-      data: pagedata,
-      content: v.drop_content,
-    };
+    });
 
-    switch (category) {
-      case "Npc":
-        handle_npc(page);
-        break;
-      case "Item":
-        handle_item(page);
-        break;
-      default:
-        console.log(`Unknown category: ${category}`);
-    }
+    // switch (page.data) {
+    //   case "Creature":
+    //     handle_npc(page);
+    //     break;
+    //   case "Items":
+    //     handle_item(page);
+    //     break;
+    //   default:
+    //     dropWarning(`Unknown category: ${category}`);
+    // }
+
+    setAttrs(
+      {
+        drop_name: "",
+        drop_data: "",
+        drop_content: "",
+        drop_category: "",
+      },
+      {
+        silent: true,
+      }
+    );
 
     // get_repeating_data(function (repeating) {
     //   var results = processDrop(page, v, repeating);
@@ -83,18 +116,16 @@ const handle_drop = (category) => {
   });
 };
 
-on("change:drop_category", (event) => {
-  handle_drop(event.newValue);
+// on("sheet:compendium-drop", (event) => {
+//   console.log(
+//     `%c Pendragon 6th Edition compendium drop`,
+//     "color: purple; font-weight:bold"
+//   );
+//   //handle_drop();
+// });
+
+["category", "name", "data", "content"].forEach((attr) => {
+  on(`change:drop_${attr}`, () => {
+    handle_drop();
+  });
 });
-
-// on("change:drop_name", (event) => {
-//   console.log(event);
-// });
-
-// on("change:drop_data", (event) => {
-//   console.log(event);
-// });
-
-// on("change:drop_content", (event) => {
-//   console.log(event);
-// });
