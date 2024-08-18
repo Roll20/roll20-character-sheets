ea4a6de77049e26e18770c7062358ebafeab2446
Corey Hickson <chickson@vendasta.com>
2024-08-18 13:17:11 -0600

    wip: fix the pip buttons

--- Torchbearer 2E/src/js/pips.js
+++ Torchbearer 2E/src/js/pips.js
@@ -1,1 +1,29 @@
-// const traitUses = ["trait"]
+const traitUses = ["trait1_uses", "trait2_uses", "trait3_uses", "trait4_uses"];
+const traitChecks = ["trait1_checks", "trait2_checks", "trait3_checks", "trait4_checks"];
+
+const abilityPasses = abilities.map(a => a + "_pass");
+const abilityFails = abilities.map(a => a + "_fail");
+
+const skillPasses = skills.map(s => s + "_pass");
+const skillFails = skills.map(s => s + "_fail");
+const skillBl = skills.map(s => s + "_bl");
+
+const customSkillPasses = [];
+const customSkillFails = [];
+const customSkillBl = [];
+
+for (let i = 1; i <= 5; i++) {
+  customSkillPasses.push = "skill" + i + "_pass"
+  customSkillPasses.push = "skill" + i + "_fail"
+  customSkillPasses.push = "skill" + i + "_bl"
+}
+
+const allListeners = [].concat(...traitUses, ...traitChecks, ...abilityPasses, ...abilityFails, ...skillPasses, ...skillFails, ...skillBl, ...customSkillPasses, ...customSkillFails, ...customSkillBl)
+
+allListeners.forEach(function (value) {
+  on(`clicked:${value}`, function (event) {
+    setAttrs({
+      [value]: event.htmlAttributes.value
+    });
+  });
+});
