892679d4214b83787afc0eb467867f960eba334b
dlm <dlm@nsi-sa.be>
2024-11-27 11:05:22 +0100

    SW5E : Import - Export Of NPC TODO : Handle Powers?

--- StarWars5E/src/js/import_export.js
+++ StarWars5E/src/js/import_export.js
@@ -1,153 +1,187 @@
 //=require attributes.js
 
-/** NPC CLEANING */
-/* Clean All attributes in npc_attrs */
-let clean_npc_attr = (callback) => {
+/* CLEANING */
+let clean_attributes = (attributesToClean, callback) => {
+    if(!attributesToClean?.length){
+        return callback();
+    }
     let update = {}
-    for(let attr of npc_attrs) {
-        update[attr] = "";
+    for(let attribute of attributesToClean) {
+        update[attribute] = "";
     }
     setAttrs(update, false,callback);
 }
 
 /* Recursive method, will clean repeating section at index i, then will call this method for index i + 1 */
-let clean_npc_repeating = (index,callback) =>{
-    if(index >= npc_repeating.length){
-        return clean_npc_attr(callback);
+let clean_sections = (sectionsToClean,index,callback) =>{
+    if(index >= sectionsToClean.length){
+        return callback();
     }
-    getSectionIDs(npc_repeating[index].section_name, (idArrays) => {
-        let update = []
-        for(let id of idArrays){
-            const toRemove = `${npc_repeating[index].section_name}_${id}`;
+    getSectionIDs(sectionsToClean[index].section_name, (sectionIds) => {
+        for(let id of sectionIds){
+            const toRemove = `${sectionsToClean[index].section_name}_${id}`;
             removeRepeatingRow(toRemove);
         }
-        clean_npc_repeating(index + 1,callback);
+        clean_sections(sectionsToClean,index + 1,callback);
     });
 }
-
-let clean_npc = () => {
-    clean_npc_repeating(0,() => {
-    });
-}
-
-/* NPC IMPORT - EXPORT */
-let import_npc = () => {
-    //Import is quite easy, clean the npc sheet then do import.
-    clean_npc_repeating(0, () => {
-        getAttrs(["json_value"], (attrs) => {
-            const jsonObject = JSON.parse(attrs["json_value"]);
-            if(typeof jsonObject.schema_version !== 'string'){
-                return import_legacy(jsonObject);
-            }
-            let toUpdate = {};
-            toUpdate["character_name"] = jsonObject.name;
-            for (let attr of jsonObject.attribs) {
-                if (!attr) {
-                    continue;
-                }
-                const name = attr.name;
-                toUpdate[name] = attr.current;
-                if (attr.max && attr.max.length) {
-                    toUpdate[`${name}_max`] = attr.max;
-                }
-            }
-            for(let section of jsonObject.sections){
-                let ids = []
-                for(let row of section.rows){
-                    let id = getUniqueRowId(ids);
-                    ids.push(id);
-                    for(let attr in row){
-                        toUpdate[`${section.section_name}_${id}_${attr}`] = row[attr];
-                    }
-                }
-            }
-            setAttrs(toUpdate, false, () => {
-                update(toUpdate["version"]);
-            });
-        });
-    });
-}
-
-let import_legacy = (jsonObject) =>{
+/* IMPORT */
+let import_legacy = (importDto) =>{
     let toUpdate = {};
-    toUpdate["character_name"] = jsonObject.name;
-    for (let attr of jsonObject.attribs) {
-        if (!attr) {
+    toUpdate["character_name"] = importDto.name;
+    for (let attribute of importDto.attribs) {
+        if (!attribute) {
             continue;
         }
-        const name = attr.name;
-        toUpdate[name] = attr.current;
-        if (attr.max && attr.max.length) {
-            toUpdate[`${name}_max`] = attr.max;
+        const name = attribute.name;
+        toUpdate[name] = attribute.current;
+        if (!!attribute.max) {
+            toUpdate[`${name}_max`] = attribute.max;
         }
     }
     setAttrs(toUpdate, false, () => {
         update(toUpdate["version"]);
     });
 }
-let export_npc_section = (index,export_value) =>{
-    if(index >= npc_repeating.length){
-        const export_string = JSON.stringify(export_value, null, 3);
-        setAttrs({"json_value":export_string});
-    }
-    getSectionIDs(npc_repeating[index].section_name, (idArrays) => {
-        let attrs = [];
-        let ids = [];
-        for(let id of idArrays) {
-            ids.push(id);
-            for (let attr of npc_repeating[index].attributes) {
-                attrs.push(`${npc_repeating[index].section_name}_${id}_${attr}`)
+
+let import_all = () => {
+    getAttrs(["json_value"], (jsonValueContainer) => {
+        const importDto = JSON.parse(jsonValueContainer["json_value"]);
+        if(typeof importDto.schema_version !== 'string'){
+            return import_legacy(importDto);
+        }
+        let toUpdate = {};
+        toUpdate["character_name"] = importDto.name;
+        for (let attribute of importDto.attribs) {
+            if (!attribute) {
+                continue;
+            }
+            const name = attribute.name;
+            toUpdate[name] = attribute.current;
+            if (!!attribute.max) {
+                toUpdate[`${name}_max`] = attribute.max;
             }
         }
-        getAttrs(attrs, (section_attrs) => {
-            let section_export = {
-                section_name : npc_repeating[index].section_name,
-                rows:[]
-            };
-            for(let id of ids){
-                const prefix = `${npc_repeating[index].section_name}_${id}_`;
-                let row_export = {};
-                let row_attr = Object.keys(section_attrs).filter( key => key.startsWith(prefix));
-                for(let attr of row_attr){
-                    row_export[attr.replace(prefix,"")] = section_attrs[attr];
+        for(let section of importDto.sections){
+            let ids = []
+            for(let row of section.rows){
+                let id = getUniqueRowId(ids);
+                ids.push(id);
+                for(let attribute in row){
+                    toUpdate[`${section.section_name}_${id}_${attribute}`] = row[attribute];
                 }
-                section_export.rows.push(row_export);
             }
-            export_value.sections.push(section_export);
-            export_npc_section(index + 1,export_value);
+        }
+        setAttrs(toUpdate, false, () => {
+            update(toUpdate["version"]);
         });
     });
 }
 
+/* EXPORT */
+let export_attributes = (attributesToExport,exportDto,callback) => {
+    //Get the values of the attributes
+    getAttrs(attributesToExport, (attributesValues) => {
+        for (let attributeName of attributesToExport) {
+            //Maximum Attributes handled already, should ignore
+            if (attributeName.endsWith("_max")) {
+                continue;
+            }
+            //Set name of export
+            if (attributeName === "character_name" && !export_value.name) {
+                exportDto.name = attributesValues[attributeName];
+                continue;
+            }
+            if (attributeName === "npc_name") {
+                exportDto.name = attributesValues[attributeName];
+            }
+            let exportedAttribute = {
+                name: attributeName,
+                current: attributesValues[attributeName],
+                max: attributesValues[`${attributeName}_max`] ? attributesValues[`${attributeName}_max`] : "",
+                id: ""
+            }
+            //Don't add empty ones
+            if(!exportedAttribute.current && !exportedAttribute.max){
+                continue;
+            }
+            exportDto.attribs.push(exportedAttribute);
+        }
+        callback();
+    });
+}
+
+let export_sections = (sectionsToExport,index,exportDto, callback) =>{
+    if(index >= sectionsToExport.length){
+        return callback();
+    }
+    //Get all row for section
+    getSectionIDs(sectionsToExport[index].section_name, (sectionIds) => {
+        //Build the list of attributes to retrieve
+        let attributesToRetrieve = [];
+        for(let id of sectionIds) {
+            for (let attr of sectionsToExport[index].attributes) {
+                attributesToRetrieve.push(`${sectionsToExport[index].section_name}_${id}_${attr}`)
+            }
+        }
+
+        getAttrs(attributesToRetrieve, (attributesValues) => {
+            //Initialize the section Export
+            let exportedSection = {
+                section_name : sectionsToExport[index].section_name,
+                rows:[]
+            };
+            //for each Id
+            for(let id of sectionIds){
+                //Calculate the prefix
+                const prefix = `${sectionsToExport[index].section_name}_${id}_`;
+                let exportedRow = {};
+                //Filter Attributes for only this row
+                let rowAttributes = Object.keys(attributesValues).filter( key => key.startsWith(prefix));
+                for(let attributeName of rowAttributes){
+                    const value = attributesValues[attributeName];
+                    //Ignore unset attributes
+                    if(!!value || typeof value === "number"){} {
+                        exportedRow[attributeName.replace(prefix, "")] = value;
+                    }
+                }
+                exportedSection.rows.push(exportedRow);
+            }
+            exportDto.sections.push(exportedSection);
+            export_sections(sectionsToExport,index + 1,exportDto,callback);
+        });
+    });
+}
+/* NPC */
+let clean_npc = (callback) => {
+    clean_sections(npc_repeating,0,() => {
+        clean_attributes(npc_attrs, () => {
+            console.log("NPC Sheet Cleaned");
+            callback();
+        });
+    });
+}
+
+let import_npc = () => {
+    //Import is quite easy, clean the npc sheet then do import.
+    clean_npc(() => {
+        import_all();
+    });
+}
+
 let export_npc = () => {
-    const export_value = {
+    const exportDto = {
         "schema_version": "Sheet-3.1",
         "exportedBy": "Roll20 Sw5E Sheet",
         "name": "",
         "attribs": [],
         "sections": []
     };
-    getAttrs(npc_attrs, (attrs) => {
-        for (let attr of npc_attrs) {
-            if (attr.endsWith("_max")) {
-                continue;
-            }
-            if (attr === "character_name" && !export_value.name) {
-                export_value.name = attrs[attr];
-                continue;
-            }
-            if(attr === "npc_name"){
-                export_value.name = attrs[attr];
-            }
-            let attrib = {
-                name: attr,
-                current: attrs[attr],
-                max: attrs[`${attr}_max`] ? attrs[`${attr}_max`] : "",
-                id: ""
-            }
-            export_value.attribs.push(attrib);
-        }
-        export_npc_section(0, export_value);
+    export_attributes(npc_attrs,exportDto, () => {
+        export_sections(npc_repeating,0,exportDto,() => {
+            const export_string = JSON.stringify(exportDto, null, 3);
+            setAttrs({"json_value":export_string});
+        });
     });
 }
 
