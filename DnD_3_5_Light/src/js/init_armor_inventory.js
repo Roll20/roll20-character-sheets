0553ef708517b67084dff65a6cbf513dce663d0a
David Lambert <40821489+BE-Arbiter@users.noreply.github.com>
2024-12-01 12:58:23 +0100

    * Feature : Global Attacks Modifiers * Improvement : Armor Layout and mechanics

--- DnD_3_5_Light/src/js/init_armor_inventory.js
+++ DnD_3_5_Light/src/js/init_armor_inventory.js
@@ -14,23 +14,27 @@ on("change:dex_mod change:init_bonus",function(){
 });
 
 /*Update armor*/
-on("change:armor_bonus change:armor_malus change:armor_max_dex change:shield_bonus change:shield_malus change:shield_equipe change:touch_bonus change:flatfooted_bonus change:universal_armor_bonus change:dex_mod",function(){
-    getAttrs(["armor_bonus","armor_malus","armor_max_dex","shield_bonus","shield_malus","shield_equipe","touch_bonus","flatfooted_bonus","universal_armor_bonus","dex_mod"],function(values){
+on("change:armor_bonus change:armor_malus change:armor_max_dex change:armor_equipe change:shield_bonus change:shield_malus change:shield_equipe change:touch_bonus change:flatfooted_bonus change:universal_armor_bonus change:dex_mod",function(){
+    getAttrs(["armor_bonus","armor_malus","armor_max_dex","armor_equipe","shield_bonus","shield_malus","shield_equipe","touch_bonus","flatfooted_bonus","universal_armor_bonus","dex_mod"],function(values){
         let armor_bonus = parseInt(values.armor_bonus) || 0;
         let armor_malus = parseInt(values.armor_malus) || 0;
         let armor_max_dex = parseInt(values.armor_max_dex) || 0;
         if(armor_max_dex < 0) armor_max_dex = 1000;
         let shield_equipe = values.shield_equipe == "on";
+        let armor_equipe = values.armor_equipe == "on";
         let shield_bonus = shield_equipe ? (parseInt(values.shield_bonus) || 0) : 0;
         let shield_malus = shield_equipe ? (parseInt(values.shield_malus) || 0) : 0;
         let touch_bonus = parseInt(values.touch_bonus) || 0;
         let flatfooted_bonus = parseInt(values.flatfooted_bonus) || 0;
         let universal_armor_bonus = parseInt(values.universal_armor_bonus) || 0;
         let dex_mod = parseInt(values.dex_mod) || 0;
-        let dex = (armor_max_dex < dex_mod) ? armor_max_dex : dex_mod;
-        let AC = 10 +armor_bonus + dex + shield_bonus + touch_bonus + flatfooted_bonus + universal_armor_bonus;
+        let dex = dex_mod
+        if(armor_equipe && armor_max_dex < dex_mod){
+            dex = armor_max_dex;
+        }
+        let AC = 10 + armor_bonus + dex + shield_bonus + touch_bonus + flatfooted_bonus + universal_armor_bonus;
         let TAC = 10 +dex + touch_bonus + universal_armor_bonus;
-        let FFAC = 10 +armor_bonus + shield_bonus + flatfooted_bonus + universal_armor_bonus;
+        let FFAC = 10 + armor_bonus + shield_bonus + flatfooted_bonus + universal_armor_bonus;
         let skill_malus = (shield_malus > 0 ? -shield_malus : shield_malus) - (armor_malus < 0 ? -armor_malus : armor_malus);
         setAttrs({
             "ac_tot":AC,
