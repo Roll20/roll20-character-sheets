4dd30fcc31169e50a4b6e3e213a8f614019123a4
vince <sefnerr@charter.net>
2025-02-27 23:30:34 -0800

    - webapack updated to minify HTML for production builds and copy css and translation files to dist and prod folders to help with roll20 sadnbox better detect changes in the custom sandbox. - dev dependencies updated - found errant newline in textarea and needed to esacpe a "less than character" that was causing minification to fail.

--- Pathfinder Community/dev/webpack.config.js
+++ Pathfinder Community/dev/webpack.config.js
@@ -4,7 +4,9 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const HtmlMinimizerPlugin = require('html-minimizer-webpack-plugin');
 const HtmlWorkerScriptPlugin = require('./HtmlWorkerScriptPlugin.js');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const webpackConfig = {
   entry: path.join(__dirname, 'src/index.js'),
@@ -31,12 +33,18 @@ const webpackConfig = {
     }),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src/index.html'),
-      inlineSource: '.js$',
+      inlineSource: /\.js$/,
       minify: false, // handled by webpack^5 optimization. see below
-      inject: false, // handled by HtmlWorkerScriptPlugin
+      inject: false, // handled by HtmlWorkerScriptPlugin.js
     }),
     new HtmlInlineScriptPlugin(),
     new HtmlWorkerScriptPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {from: path.join(__dirname, 'src/pathfinder.css'), to: path.join(__dirname, mode === 'production' ? 'prod' : 'dist')},
+        {from: path.join(__dirname, 'src/translation.json'), to: path.join(__dirname, mode === 'production' ? 'prod' : 'dist')},
+      ],
+    }),
   ],
   cache: {
     type: 'filesystem', // Recommended for production builds
@@ -51,10 +59,28 @@ const webpackConfig = {
           compress: true,
           mangle: true,
           format: {
-            comments: false, // Removes comments
+            comments: false,
           },
         },
       }),
+      ...(mode === 'production'
+        ? [
+            new HtmlMinimizerPlugin({
+              minimizerOptions: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: false,
+                minifyCSS: true,
+                minifyURLs: true,
+              },
+            }),
+          ]
+        : []),
     ],
   },
   output: {
